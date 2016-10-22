bab5e78b7137c37e4f0ee59a60607490a6c0b04f
Chris Barrett <chris.d.barrett@me.com>
2016-10-23 12:47:37 +1300

    Configure exec-path-from-shell

--- config/cb-darwin.el
+++ config/cb-darwin.el
@@ -8,11 +8,21 @@
 
 ;;; Code:
 
+(eval-when-compile
+  (require 'use-package))
+
 (global-set-key (kbd "s-q") 'save-buffers-kill-emacs)
 (global-set-key (kbd "s-v") 'yank)
 (global-set-key (kbd "s-c") 'copy-region-as-kill)
 (global-set-key (kbd "s-n") 'new-frame)
 (global-set-key (kbd "s-w") 'delete-frame)
+
+(use-package exec-path-from-shell
+  :if (memq window-system '(mac ns))
+  :config
+  (exec-path-from-shell-initialize)
+  :functions
+  (exec-path-from-shell-initialize))
 
 (provide 'cb-darwin)
 
