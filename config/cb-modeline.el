ecf468fbb8d88ac26f720f87ee1eb26259eda4ae
Chris Barrett <chris.d.barrett@me.com>
2016-10-23 12:15:03 +1300

    Factor out hidden modeline code into package

--- config/cb-modeline.el
+++ config/cb-modeline.el
@@ -8,28 +8,16 @@
 
 ;;; Code:
 
+(eval-when-compile
+  (require 'use-package))
 
-;; Hide the mode line by default.
+(use-package hidden-mode-line
+  :config
+  (global-hidden-mode-line-mode))
 
-(defvar-local hidden-mode-line-mode nil)
-(defvar-local hide-mode-line nil)
-
-(define-minor-mode hidden-mode-line-mode
-  "Minor mode to hide the mode-line."
-  :global t
-  (if hidden-mode-line-mode
-      (setq hide-mode-line mode-line-format
-            mode-line-format nil)
-    (setq mode-line-format hide-mode-line
-          hide-mode-line nil))
-  (force-mode-line-update)
-  (redraw-display))
-
-(define-globalized-minor-mode global-hidden-mode-line-mode hidden-mode-line-mode
-  hidden-mode-line-mode)
-
-(global-hidden-mode-line-mode +1)
-
+(custom-set-faces
+ `(mode-line
+   ((t (:foreground "white" :background "gray40")))))
 
 (provide 'cb-modeline)
 
