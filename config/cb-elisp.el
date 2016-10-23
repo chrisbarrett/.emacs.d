4453ed84e7006d007f7be3c69db522ed9e95fb31
Chris Barrett <chris.d.barrett@me.com>
2016-10-24 00:07:47 +1300

    Make checkdoc more permissive.

--- config/cb-elisp.el
+++ config/cb-elisp.el
@@ -93,7 +93,41 @@
   :defer t
   :after autoinsert
   :config
-  (add-to-list 'auto-insert-alist cb-elisp-autoinsert-form))
+  (add-to-list 'auto-insert-alist cb-elisp-autoinsert-form)
+  :defines (auto-insert-alist))
+
+;; Checkdoc configuration
+
+(use-package flycheck
+  :preface
+  :config
+  (setq flycheck-emacs-lisp-checkdoc-form
+        (flycheck-prepare-emacs-lisp-form
+          (setq checkdoc-force-docstrings-flag nil)
+          (setq checkdoc-arguments-in-order-flag nil)
+          (let ((source (car command-line-args-left))
+                ;; Remember the default directory of the process
+                (process-default-directory default-directory))
+            (with-temp-buffer
+              (insert-file-contents source 'visit)
+              (setq buffer-file-name source)
+              ;; And change back to the process default directory to make file-name
+              ;; back-substutition work
+              (setq default-directory process-default-directory)
+              (with-demoted-errors "Error in checkdoc: %S"
+                (checkdoc-current-buffer t)
+                (with-current-buffer checkdoc-diagnostic-buffer
+                  (princ (buffer-substring-no-properties (point-min) (point-max)))
+                  (kill-buffer)))))))
+
+  :functions (flycheck-sexp-to-string))
+
+(use-package checkdoc
+  :defer t
+  :config
+  (progn
+    (setq checkdoc-force-docstrings-flag nil)
+    (setq checkdoc-arguments-in-order-flag nil)))
 
 (provide 'cb-elisp)
 
