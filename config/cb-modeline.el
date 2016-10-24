097b2124fef8d3061db031ad9e9d1ea81348cb99
Chris Barrett <chris.d.barrett@me.com>
2016-10-24 16:09:47 +1300

    Turn the modeline into a thin grey strip

--- config/cb-modeline.el
+++ config/cb-modeline.el
@@ -12,12 +12,17 @@
   (require 'use-package))
 
 (use-package hidden-mode-line
+  :disabled t
   :config
   (global-hidden-mode-line-mode))
 
+;; Turn the modeline into a thin grey strip.
+
+(setq-default mode-line-format "")
+
 (custom-set-faces
  `(mode-line
-   ((t (:foreground "white" :background "gray40")))))
+   ((t (:foreground "white" :background "gray40" :height 20)))))
 
 (provide 'cb-modeline)
 
