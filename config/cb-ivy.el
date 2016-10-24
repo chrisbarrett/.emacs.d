37448d867e7790e61836aaba890b15e671f6ee0f
Chris Barrett <chris.d.barrett@me.com>
2016-10-24 15:28:13 +1300

    Ivy and counsel configuration

--- config/cb-ivy.el
+++ config/cb-ivy.el
@@ -33,22 +33,31 @@
   (progn
     (setq ivy-use-virtual-buffers t)
     (setq ivy-count-format "(%d/%d) ")
-    (define-key ivy-minibuffer-map (kbd "ESC") #'keyboard-escape-quit)
+    (define-key ivy-minibuffer-map (kbd "C-l") #'ivy-partial-or-done)
     (define-key ivy-minibuffer-map (kbd "S-<return>") #'cb-ivy-continue-with-input)
     (ivy-mode))
 
+  :functions (ivy-partial-or-done)
   :defines (ivy-use-virtual-buffers ivy-count-format))
 
+
+(spacemacs-keys-declare-prefix "h d" "describe")
+(spacemacs-keys-set-leader-keys
+  "h d m" #'describe-mode)
 
 (use-package counsel
   :preface
   (progn
     (autoload 'ivy-immediate-done "ivy")
+    (autoload 'counsel-up-directory "counsel")
     (autoload 'counsel-mode "counsel"))
 
   :leader-bind
   (("SPC" . counsel-M-x)
+   ("?"   . counsel-descbinds)
    ("f f" . counsel-find-file)
+   ("f r" . counsel-recentf)
+   ("i"   . counsel-imenu)
    ("h d f" . counsel-describe-function)
    ("h d v" . counsel-describe-variable))
 
@@ -62,6 +71,7 @@
   :config
   (progn
     (define-key counsel-find-file-map (kbd "C-M-j") #'ivy-immediate-done)
+    (define-key counsel-find-file-map (kbd "C-h") #'counsel-up-directory)
     (counsel-mode +1))
 
   :functions (counsel-expression-history))
