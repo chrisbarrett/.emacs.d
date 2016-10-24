ca8ad5548b009d51caf18fa83481e05a61d0cd59
Chris Barrett <chris.d.barrett@me.com>
2016-10-24 19:29:10 +1300

    Newlines continue comments

--- config/cb-basic-settings.el
+++ config/cb-basic-settings.el
@@ -41,6 +41,9 @@
 (setq scroll-margin 0)
 (setq scroll-conservatively 101)
 
+;; Auto-indent on RET
+(define-key global-map (kbd "RET") #'comment-indent-new-line)
+
 
 ;; Enable hideshow in all programming buffers.
 
