e34f23bda4904024b4e744ad99857adce887474d
Chris Barrett <chris.d.barrett@me.com>
2016-10-24 20:53:05 +1300

    Fix missing ivy escape binding

--- config/cb-ivy.el
+++ config/cb-ivy.el
@@ -36,6 +36,7 @@
     (setq ivy-use-virtual-buffers t)
     (setq ivy-count-format "(%d/%d) ")
     (define-key ivy-occur-mode-map (kbd "C-x C-w") #'ivy-wgrep-change-to-wgrep-mode)
+    (define-key ivy-minibuffer-map (kbd "<escape>") 'minibuffer-keyboard-quit)
     (define-key ivy-minibuffer-map (kbd "C-l") #'ivy-partial-or-done)
     (define-key ivy-minibuffer-map (kbd "C-c C-e") #'cb-ivy-occur-then-wgrep)
     (define-key ivy-minibuffer-map (kbd "S-<return>") #'cb-ivy-continue-with-input)
