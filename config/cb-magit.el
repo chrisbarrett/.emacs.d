3df3b6ab9ac97d36bd6221faef7981b40ca99746
Chris Barrett <chris.d.barrett@me.com>
2016-10-28 22:49:02 +1300

    Add git time machine

--- config/cb-magit.el
+++ config/cb-magit.el
@@ -13,6 +13,7 @@
   (require 'cb-use-package-extensions))
 
 (require 'spacemacs-keys)
+(require 'evil-transient-state)
 
 (use-package magit
   :defer t
@@ -42,6 +43,39 @@
   :commands (git-auto-commit-mode)
   :init
   (add-to-list 'safe-local-variable-values '(gac-automatically-push-p . t)))
+
+(use-package git-timemachine
+  :defer t
+  :commands
+  (git-timemachine
+   git-timemachine-show-current-revision
+   git-timemachine-show-nth-revision
+   git-timemachine-show-previous-revision
+   git-timemachine-show-next-revision
+   git-timemachine-show-previous-revision
+   git-timemachine-kill-revision
+   git-timemachine-quit)
+  :preface
+  (evil-transient-state-define time-machine
+    :title "Git Timemachine Transient State"
+    :doc "
+[_p_/_N_] previous [_n_] next [_c_] current [_g_] goto nth rev [_Y_] copy hash [_q_] quit"
+        :on-enter (unless (bound-and-true-p git-timemachine-mode)
+                      (call-interactively 'git-timemachine))
+        :on-exit (when (bound-and-true-p git-timemachine-mode)
+                   (git-timemachine-quit))
+        :foreign-keys run
+        :bindings
+        ("c" git-timemachine-show-current-revision)
+        ("g" git-timemachine-show-nth-revision)
+        ("p" git-timemachine-show-previous-revision)
+        ("n" git-timemachine-show-next-revision)
+        ("N" git-timemachine-show-previous-revision)
+        ("Y" git-timemachine-kill-revision)
+        ("q" nil :exit t))
+  :init
+  (spacemacs-keys-set-leader-keys
+    "gt" #'time-machine-transient-state/body))
 
 (provide 'cb-magit)
 
