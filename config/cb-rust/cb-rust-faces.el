4da5f27bfb9d72c2dc5a27c3fc0cca2aef76a2f2
Chris Barrett <chris.d.barrett@me.com>
2016-11-01 09:49:56 +1300

    Tweak font lock for static scope references

--- config/cb-rust/cb-rust-faces.el
+++ config/cb-rust/cb-rust-faces.el
@@ -30,12 +30,17 @@
 (defconst cb-rust-faces--bool-rx
   (rx word-start (group (or "true" "false")) word-end))
 
+(defconst cb-rust-faces--static-scope-rx
+  (rx "'" word-start (group "static") word-end))
 
 (font-lock-add-keywords
  'rust-mode
  `(
    ;; Apply face to distinguish macro invocations from directives.
    (,cb-rust-faces--macro-rx 1 'cb-rust-faces-macro)
+
+   ;; Remove keyword face from references to `static' scope.
+   (,cb-rust-faces--static-scope-rx 1 'font-lock-variable-name-face)
 
    ;; Distinguish `true' and `false' literals from other keywords.
    (,cb-rust-faces--bool-rx 1 'cb-rust-faces-bool)))
