e47cb02774aa04ac52c7b95ebaa42f0fa55d8401
Chris Barrett <chris.d.barrett@me.com>
2016-11-06 10:20:37 +1300

    Customise dired

--- config/cb-dired.el
+++ config/cb-dired.el
@@ -19,18 +19,35 @@
 (use-package dired
   :defer t
   :commands (dired dired-hide-details-mode)
+  :preface
+
+  (defun cb-dired--sort-directories-first (&rest _)
+    "Sort dired listings with directories first."
+    (save-excursion
+      (let (buffer-read-only)
+        (forward-line 2) ;; beyond dir. header
+        (sort-regexp-fields t "^.*$" "[ ]*." (point) (point-max)))
+      (set-buffer-modified-p nil)))
+
   :init
   (progn
     (spacemacs-keys-declare-prefix-for-mode 'dired-mode "s" "subdir")
     (spacemacs-keys-set-leader-keys "d" #'dired)
     (spacemacs-keys-set-leader-keys-for-major-mode
       'dired-mode
+      "d"  'dired-hide-details-mode
       "si" 'dired-insert-subdir
       "sd" 'dired-kill-subdir)
     (add-hook 'dired-mode-hook #'dired-hide-details-mode))
+
   :config
-  (unless (bound-and-true-p diredp-loaded-p)
-    (load-file (concat cb-emacs-lisp-directory "/dired-plus/dired+.el"))))
+  (progn
+    (setq-default dired-listing-switches "-alhv")
+    (setq dired-dwim-target t)
+    (advice-add 'dired-readin :after #'cb-dired--sort-directories-first)
+
+    (unless (bound-and-true-p diredp-loaded-p)
+      (load-file (concat cb-emacs-lisp-directory "/dired-plus/dired+.el")))))
 
 (use-package dired-x
   :commands (dired-omit-mode)
@@ -45,6 +62,7 @@
   :config
   (progn
     (evil-define-key 'normal dired-mode-map (kbd "h") #'dired-omit-mode)
+    (setq dired-clean-up-buffers-too t)
     (setq dired-omit-files (rx bol (+ ".")))))
 
 
