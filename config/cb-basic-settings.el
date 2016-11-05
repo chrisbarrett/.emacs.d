af5cd9de3e759d57e90fcce54c77a814f3705cc4
Chris Barrett <chris.d.barrett@me.com>
2016-11-06 10:53:27 +1300

    Sentence end double-space

--- config/cb-basic-settings.el
+++ config/cb-basic-settings.el
@@ -233,6 +233,7 @@ Optional arg JUSTIFY will justify comments and strings."
 (setq-default fill-column 80)
 (setq-default tab-width 4)
 (setq-default indent-tabs-mode nil)
+(setq-default sentence-end-double-space t)
 
 (setq sentence-end-double-space nil)
 (setq delete-by-moving-to-trash nil)
