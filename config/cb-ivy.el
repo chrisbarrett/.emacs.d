180e7574667ca3ec14902b6046084c4c680895a3
Chris Barrett <chris.d.barrett@me.com>
2016-11-06 00:04:46 +1300

    Use helm-style regexes for ivy

--- config/cb-ivy.el
+++ config/cb-ivy.el
@@ -44,7 +44,7 @@
 
     (setq ivy-use-virtual-buffers t)
     (setq ivy-count-format "(%d/%d) ")
-    (setq ivy-re-builders-alist '((t . ivy--regex-fuzzy)))
+    (setq ivy-re-builders-alist '((t . ivy--regex-plus)))
 
     (define-key ivy-minibuffer-map (kbd "<f1>") #'cb-ivy-help)
     (define-key ivy-occur-mode-map (kbd "C-x C-w") #'ivy-wgrep-change-to-wgrep-mode)
