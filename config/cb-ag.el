9de87ffd48f9a717a0aa5deb145bce07472f9a7d
Chris Barrett <chris.d.barrett@me.com>
2016-11-08 21:08:17 +1300

    Kill buffer after completing wgrep

--- config/cb-ag.el
+++ config/cb-ag.el
@@ -14,6 +14,24 @@
 (use-package ag
   :commands ag)
 
+(use-package wgrep
+  :defer t
+  :preface
+  (progn
+    (autoload 'wgrep-finish-edit "wgrep")
+
+    (defun cb-ag-wgrep-finish-edit-kill-buffer ()
+      "Finish the current wgrep edit and kill the wgrep buffer."
+      (interactive)
+      (let ((buf (current-buffer)))
+        (prog1 (wgrep-finish-edit)
+          (kill-buffer buf)))))
+
+  :config
+  (progn
+    (setq wgrep-auto-save-buffer t)
+    (define-key wgrep-mode-map [remap wgrep-finish-edit] #'cb-ag-wgrep-finish-edit-kill-buffer)))
+
 (use-package wgrep-ag
   :after ag)
 
