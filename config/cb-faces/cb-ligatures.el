80c7dbeddc9cd1bc772f0f6a1e5e3f88e8513a5a
Chris Barrett <chris.d.barrett@me.com>
2016-11-22 20:14:25 +1300

    Fix email address in package header

--- config/cb-faces/cb-ligatures.el
+++ config/cb-faces/cb-ligatures.el
@@ -2,7 +2,7 @@
 
 ;; Copyright (C) 2016  Chris Barrett
 
-;; Author: Chris Barrett <chris.d.barrett@me.com>
+;; Author: Chris Barrett <chris+emacs@walrus.cool>
 ;; Package-Requires: ((dash "2.12.1"))
 
 ;; This program is free software; you can redistribute it and/or modify
