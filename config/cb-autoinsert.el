82874de4a0653d08fc93d2cf928fb0b1d9c33295
Chris Barrett <chris.d.barrett@me.com>
2017-01-01 23:30:22 +1300

    Start in insert state after auto-insert runs

--- config/cb-autoinsert.el
+++ config/cb-autoinsert.el
@@ -11,13 +11,23 @@
 (eval-when-compile
   (require 'use-package))
 
+(autoload 'evil-insert-state "evil-states")
+(autoload 'string-empty-p "subr-x")
+
 (use-package autoinsert
   :init
   (defvar auto-insert-alist nil)
+  :preface
+  (defun cb-autoinsert--maybe-enter-insert-state (fn &rest args)
+    (let ((new-file? (string-empty-p (buffer-string))))
+      (apply fn args)
+      (when new-file?
+        (evil-insert-state))))
   :config
   (progn
     (setq auto-insert-query nil)
-    (auto-insert-mode +1)))
+    (auto-insert-mode +1)
+    (advice-add 'auto-insert :around #'cb-autoinsert--maybe-enter-insert-state)))
 
 (provide 'cb-autoinsert)
 
