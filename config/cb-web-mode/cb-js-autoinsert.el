48cbbe0dcc954240bbc3c8ad67e874c20b919b47
Chris Barrett <chris.d.barrett@me.com>
2017-01-04 14:45:57 +1300

    JS templates differ for container vs component files

--- config/cb-web-mode/cb-js-autoinsert.el
+++ config/cb-web-mode/cb-js-autoinsert.el
@@ -17,43 +17,100 @@
 
 (autoload 'yas-expand-snippet "yasnippet")
 
-(defun cb-js-autoinsert--component-name (file)
+(defun cb-js-autoinsert--container-name (file)
   (s-upper-camel-case (f-no-ext (f-filename file))))
 
+(defun cb-js-autoinsert--component-name (file)
+  (let ((container-name (s-upper-camel-case (f-no-ext (f-filename file)))))
+    (s-chop-suffix "Component" container-name)))
 
-;; React components
 
-(defun cb-js-autoinsert--flow-react-component (file)
-  (format "
+;; React components and containers
+
+(defun cb-js-autoinsert--flow-react-container (file)
+  (let ((container-name (cb-js-autoinsert--container-name file))
+        (component-name (cb-js-autoinsert--component-name file)))
+    (concat "
 
 // @flow
 import React from 'react';
 
-type Props = {}
+type Props = {| |}
 
-const ${1:%s} = ({}: Props) => (
-  $0
-)
+const " component-name " = (props: Props) => (
+  <p>" component-name "</p>
+);
 
-export default $1;
+class " container-name " extends React.Component {
+  props: Props;
 
-" (cb-js-autoinsert--component-name file)))
+  render = () => {
+    return (
+      <" component-name " $0 />
+    )
+  };
+}
 
+export default " container-name ";
 
-(defun cb-js-autoinsert--react-component (file)
-  (format "
+")))
+
+(defun cb-js-autoinsert--flow-react-component (file)
+  (let ((component-name (cb-js-autoinsert--component-name file)))
+    (concat "
+
+// @flow
+import React from 'react';
+
+type Props = {| |}
+
+const " component-name " = (props: Props) => (
+  <p>$0</p>
+);
+
+export default " component-name ";
+
+")))
+
+(defun cb-js-autoinsert--react-container (file)
+  (let ((container-name (cb-js-autoinsert--container-name file))
+        (component-name (cb-js-autoinsert--component-name file)))
+    (concat "
 
 import React from 'react';
 
-const ${1:%s} = (props) => (
-  $0
-)
+const " component-name " = (props) => (
+  <p>" component-name "</p>
+);
 
-export default $1;
+class " container-name " extends React.Component {
+  render = () => {
+    return (
+      <" component-name " $0 />
+    )
+  };
+}
 
-" (cb-js-autoinsert--component-name file)))
+export default " container-name ";
+
+")))
+
+(defun cb-js-autoinsert--react-component (file)
+  (let ((component-name (cb-js-autoinsert--container-name file)))
+    (concat "
+
+import React from 'react';
+
+const " component-name " = (props) => (
+  <p>$0</p>
+);
+
+export default " component-name ";
+
+")))
 
 (defun cb-js-autoinsert--flow-standard-source ()
+  nil
   "
 // @flow
 
@@ -65,17 +122,23 @@ $0
 ;; Expansion function
 
 (defun cb-js-autoinsert-template-string ()
-  (let ((file (buffer-file-name)))
+  (let* ((file (buffer-file-name))
+         (container-file-name? (s-suffix? "Container" (f-no-ext (f-filename file))))
+         (in-components? (member "components" (f-split file)))
+         (flow-project? (locate-dominating-file file ".flowconfig")))
     (when-let (snippet
                (cond
-                ((and (member "components" (f-split file))
-                      (locate-dominating-file file ".flowconfig"))
+                ((and in-components? flow-project? container-file-name?)
+                 (cb-js-autoinsert--flow-react-container file))
+                ((and in-components? flow-project?)
                  (cb-js-autoinsert--flow-react-component file))
 
-                ((member "components" (f-split file))
-                 (cb-js-autoinsert--react-component file))
+                ((and in-components? container-file-name?)
+                 (cb-js-autoinsert--flow-react-container file))
+                (in-components?
+                 (cb-js-autoinsert--flow-react-component file))
 
-                ((locate-dominating-file file ".flowconfig")
+                (flow-project?
                  (cb-js-autoinsert--flow-standard-source))))
 
       (yas-expand-snippet (s-trim snippet)))))
