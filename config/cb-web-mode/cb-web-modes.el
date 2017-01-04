d6e972038913d778f05d33b5c9ea06fd77eeb089
Chris Barrett <chris.d.barrett@me.com>
2017-01-04 14:45:38 +1300

    Improve setup for modes derived from web-mode

--- config/cb-web-mode/cb-web-modes.el
+++ config/cb-web-mode/cb-web-modes.el
@@ -16,15 +16,18 @@
 
 ;;;###autoload
 (define-derived-mode cb-web-json-mode web-mode "JSON"
-  "Derived mode for editing JSON files.")
+  "Derived mode for editing JSON files."
+  (setq-local web-mode-content-type "json"))
 
 ;;;###autoload
 (define-derived-mode cb-web-html-mode web-mode "HTML"
-  "Derived mode for editing HTML files.")
+  "Derived mode for editing HTML files."
+  (setq-local web-mode-content-type "html"))
 
 ;;;###autoload
 (define-derived-mode cb-web-css-mode web-mode "CSS"
-  "Derived mode for editing CSS files.")
+  "Derived mode for editing CSS files."
+  (setq-local web-mode-content-type "css"))
 
 (provide 'cb-web-modes)
 
