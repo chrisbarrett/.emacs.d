cfd359ee5fc6df0338acc16f0fbef5f843eccda5
Chris Barrett <chris.d.barrett@me.com>
2017-01-25 13:38:56 +1300

    Don't enter insert state after autoinserting

--- config/cb-autoinsert.el
+++ config/cb-autoinsert.el
@@ -17,17 +17,10 @@
 (use-package autoinsert
   :init
   (defvar auto-insert-alist nil)
-  :preface
-  (defun cb-autoinsert--maybe-enter-insert-state (fn &rest args)
-    (let ((new-file? (string-empty-p (buffer-string))))
-      (apply fn args)
-      (when new-file?
-        (evil-insert-state))))
   :config
   (progn
     (setq auto-insert-query nil)
-    (auto-insert-mode +1)
-    (advice-add 'auto-insert :around #'cb-autoinsert--maybe-enter-insert-state)))
+    (auto-insert-mode +1)))
 
 (provide 'cb-autoinsert)
 
