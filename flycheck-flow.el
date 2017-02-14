28b85b946a033efe29f865aeed9240c3b18a4b0e
Chris Barrett <chris.d.barrett@me.com>
2017-02-14 23:03:50 +1300

    Squashed 'lisp/emacs-flycheck-flow/' changes from 0748aa26a..090e89455
    090e89455 Merge pull request #21 from jedahu/master
    5d23ff0f6 javscript-flow-coverage: include endo of region in message
    979f7f123 Have javascript-flow-coverage use flycheck-javascript-flow-args
    4db905dfb Add --quiet to flow commands
    da2d0834e Merge pull request #20 from jedahu/master
    a1ebd79f3 Add coverage checker (using `flow coverage`)
    
    git-subtree-dir: lisp/emacs-flycheck-flow
    git-subtree-split: 090e89455b1f1dcb2de7a2f40c36d2a002417795

--- flycheck-flow.el
+++ flycheck-flow.el
@@ -4,8 +4,8 @@
 ;;
 ;; Author: Lorenzo Bolla <lbolla@gmail.com>
 ;; Created: 16 Septermber 2015
-;; Version: 1.0
-;; Package-Requires: ((flycheck "0.18"))
+;; Version: 1.1
+;; Package-Requires: ((flycheck "0.18") (json "1.4"))
 
 ;;; Commentary:
 
@@ -20,6 +20,9 @@
 ;; You want to use flow in conjunction with other JS checkers.
 ;; E.g. to use with gjslint, add this to your init.el
 ;; (flycheck-add-next-checker 'javascript-gjslint 'javascript-flow)
+
+;; For coverage warnings add this to your init.el
+;; (flycheck-add-next-checker 'javascript-flow 'javascript-flow-coverage)
 
 ;;; License:
 
@@ -43,9 +46,16 @@
 
 ;;; Code:
 (require 'flycheck)
+(require 'json)
 
 (flycheck-def-args-var flycheck-javascript-flow-args javascript-flow)
 (customize-set-variable 'flycheck-javascript-flow-args '())
+
+(defun flycheck-flow--predicate ()
+  (and
+   buffer-file-name
+   (file-exists-p buffer-file-name)
+   (locate-dominating-file buffer-file-name ".flowconfig")))
 
 (flycheck-define-checker javascript-flow
     "A JavaScript syntax and style checker using Flow.
@@ -55,16 +65,12 @@ See URL `http://flowtype.org/'."
               "flow"
               "check-contents"
               (eval flycheck-javascript-flow-args)
+              "--quiet"
               "--from" "emacs"
               "--color=never"
               source-original)
     :standard-input t
-    :predicate
-    (lambda ()
-      (and
-       buffer-file-name
-       (file-exists-p buffer-file-name)
-       (locate-dominating-file buffer-file-name ".flowconfig")))
+    :predicate flycheck-flow--predicate
     :error-patterns
     ((error line-start
             (file-name)
@@ -75,7 +81,46 @@ See URL `http://flowtype.org/'."
             line-end))
     :modes (js-mode js2-mode js3-mode))
 
+(flycheck-define-checker javascript-flow-coverage
+  "A coverage checker for Flow.
+
+See URL `http://flowtype.org/'."
+  :command (
+            "flow"
+            "coverage"
+            (eval flycheck-javascript-flow-args)
+            "--quiet"
+            "--json"
+            "--from" "emacs"
+            "--path" source-original)
+  :standard-input t
+  :predicate flycheck-flow--predicate
+  :error-parser
+  (lambda (output checker buffer)
+    (let* ((json-array-type 'list)
+           (json-object-type 'alist)
+           (locs (condition-case nil
+                     (let ((report (json-read-from-string output)))
+                       (alist-get 'uncovered_locs (alist-get 'expressions report)))
+                   (error nil))))
+      (mapcar (lambda (loc)
+                (let ((start (alist-get 'start loc))
+                      (end (alist-get 'end loc)))
+                  (flycheck-error-new
+                   :buffer buffer
+                   :checker 'javascript-flow-coverage
+                   :filename buffer-file-name
+                   :line (alist-get 'line start)
+                   :column (alist-get 'column start)
+                   :message (format "no-coverage-to (%s . %s)"
+                                    (alist-get 'line end)
+                                    (alist-get 'column end))
+                   :level 'warning)))
+              locs)))
+  :modes (js-mode js2-mode js3-mode))
+
 (add-to-list 'flycheck-checkers 'javascript-flow)
+(add-to-list 'flycheck-checkers 'javascript-flow-coverage t)
 
 (provide 'flycheck-flow)
 ;;; flycheck-flow.el ends here
