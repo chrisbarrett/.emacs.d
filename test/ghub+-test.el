4926999b63632d6b4f9ddf2da6b6c4213d8003aa
Chris Barrett <chris+git@walrus.cool>
2017-05-18 21:19:47 +1200

    Squashed 'lisp/ghub-plus/' content from commit 07bd117a7
    git-subtree-dir: lisp/ghub-plus
    git-subtree-split: 07bd117a77d2f2de88facfa18b839c5c8bd5a423

---
+++ test/ghub+-test.el
@@ -0,0 +1,49 @@
+(require 'ghub)
+(require 'ghub+)
+
+;;; No apparent way to provide testing for authenticated requests
+(setq ghub-authenticate nil)
+
+(ert-deftest basic ()
+  (should
+   (let* ((repo '((owner (login . "vermiculus"))
+                  (name . "ghub-plus")))
+          (repo  (ghubp-get-repos-owner-repo repo)))
+     (= 82884749 (alist-get 'id repo)))))
+
+(ert-deftest lint-unused-args ()
+  (should (lint "ghub+.el" #'lint-unused-args 'per-form)))
+
+(ert-deftest lint-undeclared-args ()
+  (should (lint "ghub+.el" #'lint-undeclared-standard-args)))
+
+(ert-deftest lint-ext-reference-in-name ()
+  (should (lint "ghub+.el" #'lint-ext-reference-in-name 'per-form)))
+
+(ert-deftest linter-selftest ()
+  (message ">>> Start linter self-tests")
+
+  (should (lint-unused-args '(defapiget-ghubp "/rate_limit" "" "" (repo issue) "/:repo.thing")))
+  (should (lint-unused-args '(defapiget-ghubp "/rate_limit" "" "" (repo) "/:repo.thing/:issue.thing")))
+  (should (lint-ext-reference-in-name '(defapiget-ghubp "/rate.limit" "" "" (repo) "/:repo.thing/:issue.thing")))
+  (should-not (lint-unused-args '(defapiget-ghubp "/some_call_with_no_args" "some-desc" "some-url"
+                                   :post-process (lambda (o) (ghubp--post-process o '(subject))))))
+  (should-not (lint-unused-args '(defapiget-ghubp "/some_call_with_no_args" "some-desc" "some-url")))
+
+  (should (lint-standard-args-undeclared--internal
+           '(defapiget-ghubp "/some_call_with_new_args" "some-doc" "some-link" (repo issue weird) "")
+           '(repo issue)))
+  (should-not (lint-standard-args-undeclared--internal
+               '(defapiget-ghubp "/some_call_with_new_args" "some-doc" "some-link" (repo issue) "")
+               '(repo issue)))
+  (should-not (lint-standard-args-undeclared--internal
+               '(defapiget-ghubp "/some_call_with_new_args" "some-doc" "some-link" (repo) "")
+               '(repo issue)))
+  (should-not (lint-standard-args-undeclared--internal
+               '(defapiget-ghubp "/some_call_with_new_args" "some-doc" "some-link" "")
+               '(repo issue)))
+  (should-not (lint-standard-args-undeclared--internal
+               '(defapiget-ghubp "/some_call_with_new_args" "some-doc" "some-link")
+               '(repo issue)))
+
+  (message "<<< End linter self-tests"))
