e3fc8e55343eebb9315767ca0f861b391b55cd20
Chris Barrett <chris+git@walrus.cool>
2017-06-04 21:17:08 +1200

    Add Scala Repl to display-buffer-alist

--- config/cb-scala.el
+++ config/cb-scala.el
@@ -242,13 +242,23 @@
                    (window-height   . 0.2)))
 
     (add-to-list 'display-buffer-alist
-                 `(,(rx bos "*Scala REPL" "*" eos)
+                 `(,(rx bos "*sbt*" (+ nonl) eos)
                    (display-buffer-reuse-window
                     display-buffer-in-side-window)
                    (reusable-frames . visible)
                    (side            . bottom)
                    (slot            . 0)
                    (window-height   . 0.2)))
+
+    (add-to-list 'display-buffer-alist
+                 `(,(rx bos "*Scala REPL*" eos)
+                   (display-buffer-reuse-window
+                    display-buffer-in-side-window)
+                   (reusable-frames . visible)
+                   (side            . bottom)
+                   (slot            . 0)
+                   (window-height   . 0.2)))
+
 
     (add-to-list 'display-buffer-alist
                  `(,(rx bos "*ensime-gen-config*" eos)
