5e639828ef0108fbd39f5e79232ff00d67c10a44
Chris Barrett <chris+git@walrus.cool>
2017-07-11 20:07:38 +1200

    Squashed 'lisp/mu/' content from commit 4014e3d210
    git-subtree-dir: lisp/mu
    git-subtree-split: 4014e3d210bed5ad639a3a31ac34502e81f2290a

---
+++ guile/mu/plot.scm
@@ -0,0 +1,80 @@
+;;
+;; Copyright (C) 2011-2013 Dirk-Jan C. Binnema <djcb@djcbsoftware.nl>
+;;
+;; This program is free software; you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by the
+;; Free Software Foundation; either version 3, or (at your option) any
+;; later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program; if not, write to the Free Software Foundation,
+;; Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+(define-module (mu plot)
+  :use-module (mu)
+  :use-module (ice-9 popen)
+  :export ( mu:plot ;; alias for mu:plot-histogram
+	    mu:plot-histogram
+	    ))
+
+(define (export-pairs pairs)
+  "Write a temporary file with the list of PAIRS in table format, and
+return the file name."
+  (let* ((datafile (tmpnam))
+	  (output (open datafile (logior O_CREAT O_WRONLY) #O0600)))
+    (for-each
+      (lambda(pair)
+	(display (format #f "~a ~a\n" (car pair) (cdr pair)) output))
+      pairs)
+    (close output)
+    datafile))
+
+(define (find-program-in-path prog)
+  "Find exutable program PROG in PATH; return the full path, or #f if
+not found."
+  (let* ((path (parse-path (getenv "PATH")))
+	  (progpath (search-path path prog)))
+    (if (not progpath)
+      #f
+      (if (access? progpath X_OK) ;; is
+	progpath
+	#f))))
+
+(define* (mu:plot-histogram data title x-label y-label
+	   #:optional (output "dumb") (extra-gnuplot-opts '()))
+  "Plot DATA with TITLE, X-LABEL and X-LABEL using the gnuplot
+program. DATA is a list of cons-pairs (X . Y).
+
+ OUTPUT is a string
+that determines the type of output that gnuplot produces, depending on
+the system. Which options are available depends on the particulars for
+the gnuplot installation, but typical examples would be \"dumb\" for
+text-only display, \"wxterm\" to write to a graphical window, or
+\"png\" to write a PNG-image to stdout.
+
+EXTRA-GNUPLOT-OPTS is a list
+of any additional options for gnuplot."
+  (if (not (find-program-in-path "gnuplot"))
+    (error "cannot find 'gnuplot' in path"))
+  (let ((datafile (export-pairs data))
+	 (gnuplot (open-pipe "gnuplot -p" OPEN_WRITE)))
+    (display (string-append
+	       "reset\n"
+	       "set term " (or output "dumb") "\n"
+	       "set title \"" title "\"\n"
+	       "set xlabel \"" x-label "\"\n"
+	       "set ylabel \"" y-label "\"\n"
+	       "set boxwidth 0.9\n"
+	       (string-join extra-gnuplot-opts "\n")
+	       "plot \"" datafile "\" using 2:xticlabels(1) with boxes fs solid\n")
+      gnuplot)
+    (close-pipe gnuplot)))
+
+;; backward compatibility
+(define mu-plot mu:plot-histogram)
---
+++ guile/mu/script.scm
@@ -0,0 +1,57 @@
+;; Copyright (C) 2012-2013 Dirk-Jan C. Binnema <djcb@djcbsoftware.nl>
+;;
+;; This program is free software; you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by the
+;; Free Software Foundation; either version 3, or (at your option) any
+;; later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program; if not, write to the Free Software Foundation,
+;; Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+(define-module (mu script)
+  :export (mu:run-stats))
+
+(use-modules (ice-9 getopt-long) (ice-9 optargs) (ice-9 popen) (ice-9 format))
+(use-modules (mu) (mu stats) (mu plot))
+
+(define (help-and-exit)
+  "Show some help."
+  (display
+    (string-append "usage: script [--help] [--textonly] "
+      "[--muhome=<muhome>] [--query=<query>")
+    (newline))
+  (exit 0))
+
+(define (mu:run-stats args func)
+  "Run some statistics function.
+Interpret argument-list ARGS (like command-line
+arguments). Possible arguments are:
+  --help (show some help and exit)
+  --muhome (path to alternative mu home directory)
+  --output (a string describing the output, e.g. \"dumb\", \"png\" \"wxt\")
+  searchexpr (a search query)
+then call FUNC with args SEARCHEXPR and OUTPUT."
+  (setlocale LC_ALL  "")
+  (let* ((optionspec   '((muhome  (value #t))
+			  (query   (value #t))
+			  (output  (value #f))
+			  (help    (single-char #\h) (value #f))))
+	  (options (getopt-long args optionspec))
+	  (query (option-ref options 'query #f))
+	  (help (option-ref options 'help #f))
+	  (output (option-ref options 'output #f))
+	  (muhome (option-ref options 'muhome #f))
+	  (restargs (option-ref options '() #f)))
+    (if help (help-and-exit))
+    (mu:initialize muhome)
+    (func (or query "") output)))
+
+;; Local Variables:
+;; mode: scheme
+;; End:
