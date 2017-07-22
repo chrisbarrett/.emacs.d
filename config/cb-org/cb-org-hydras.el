8c3975b44c1ce282e95d663c1b7a6c73de57d727
Chris Barrett <chris+git@walrus.cool>
2017-07-23 09:36:01 +1200

    Update org hydras

--- config/cb-org/cb-org-hydras.el
+++ config/cb-org/cb-org-hydras.el
@@ -9,7 +9,7 @@
 
 (defhydra cb-org-babel ()
   "
-[_i_] block info         [_?_] check header       [_z_] show code and session
+[_i_] block info         [_?_] check header
 [_x_] split/insert new   [_I_] insert header arg  [_q_] quit
 
 Execute   ^^              Navigate        ^^^^               Tangle    ^^
@@ -18,9 +18,12 @@ Execute   ^^              Navigate        ^^^^               Tangle    ^^
 [_b_] buffer              [_h_] head                      ^^ [_c_] clean
 [_s_] subtree             [_u_] up heading                ^^ [_d_] detangle
 [_k_] delete result                                     ^^^^ [_j_] jump to org file
-         ^^               [_B_] goto named block
-         ^^               [_N_] goto named result
-         ^^               [_r_] goto this block's result
+          ^^              [_B_] goto named block
+          ^^              [_N_] goto named result
+          ^^              [_r_] goto this block's result
+Sessions
+───────────^^^^^^──────────────────────────────────────────────────
+[_z_] show session   [_Z_] show code and session   [_l_] load block
 "
   ("?" org-babel-check-src-block :exit t)
   ("B" org-babel-goto-named-src-block)
@@ -33,6 +36,7 @@ Execute   ^^              Navigate        ^^^^               Tangle    ^^
   ("h" org-babel-goto-src-block-head)
   ("i" org-babel-view-src-block-info :exit t)
   ("j" org-babel-tangle-jump-to-org :exit t)
+  ("l" org-babel-load-in-session)
   ("k" org-babel-remove-result)
   ("n" org-babel-next-src-block)
   ("p" org-babel-previous-src-block)
@@ -41,7 +45,8 @@ Execute   ^^              Navigate        ^^^^               Tangle    ^^
   ("t" org-babel-tangle :exit t)
   ("u" outline-up-heading)
   ("x" org-babel-demarcate-block :exit t)
-  ("z" org-babel-switch-to-session-with-code :exit t)
+  ("z" org-babel-switch-to-session :exit t)
+  ("Z" org-babel-switch-to-session-with-code :exit t)
   ("q" hydra-keyboard-quit :exit t))
 
 (provide 'cb-org-hydras)
