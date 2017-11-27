ae9ec46d0655ca77789fe8469155ee13e8849bc6
Chris Barrett <chris+git@walrus.cool>
2017-11-28 10:28:35 +1300

    More fstar bindings

--- config/cb-fstar.el
+++ config/cb-fstar.el
@@ -18,17 +18,19 @@
     (evil-define-key 'normal fstar-mode-map (kbd "K") 'fstar-doc-at-point-dwim)
 
     (spacemacs-keys-set-leader-keys-for-major-mode 'fstar-mode
-      "E" 'fstar-eval-custom
       "W" 'fstar-browse-wiki-in-browser
       "d" 'fstar-visit-dependency
-      "e" 'fstar-eval
+      "ec" 'fstar-eval-custom
+      "ee" 'fstar-eval
       "h" 'fstar-doc
       "l" 'fstar-outline
       "o" 'fstar-list-options
       "p" 'fstar-print
       "q" 'fstar-quit-windows
+      "r" 'fstar-subp-reload-to-point
       "s" 'fstar-insert-match-dwim
       "v" 'fstar-cli-verify
+      "SPC" 'fstar-subp-advance-or-retract-to-point
       "w" 'fstar-browse-wiki
       "/" 'fstar-search)))
 
