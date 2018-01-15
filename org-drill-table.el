1fedd7b0c9fa94e2f6a690f64b89efd08e6c8bc3
Chris Barrett <chris+git@walrus.cool>
2018-01-15 23:02:09 +1300

    Squashed 'lisp/org-drill-table/' changes from 5662511e98..f8fdf4bfca
    f8fdf4bfca Loosen orgmode package requirements for org-drill-table (#10)
    
    git-subtree-dir: lisp/org-drill-table
    git-subtree-split: f8fdf4bfcad5de7d80a7ad0ae3327e3d36233c80

--- org-drill-table.el
+++ org-drill-table.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2014 Chris Barrett
 
 ;; Author: Chris Barrett <chris.d.barrett@me.com>
-;; Package-Requires: ((s "1.7.0") (dash "2.2.0") (cl-lib "0.3") (org-plus-contrib "8.2") (emacs "24.1"))
+;; Package-Requires: ((s "1.7.0") (dash "2.2.0") (cl-lib "0.3") (org "8.2") (emacs "24.1"))
 ;; Version: 0.1.1
 
 ;; This file is not part of GNU Emacs.
