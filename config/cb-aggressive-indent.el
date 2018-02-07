0a972ecfeb9685847ec112d53ae209a6478d7d07
Chris Barrett <chris+git@walrus.cool>
2018-02-07 23:38:53 +1300

    Forward-define aggressive-indent-excluded-modes

--- config/cb-aggressive-indent.el
+++ config/cb-aggressive-indent.el
@@ -14,25 +14,33 @@
 (use-package aggressive-indent
   :commands (global-aggressive-indent-mode)
   :defer 1
-  :config
-  (progn
-    (add-to-list 'aggressive-indent-excluded-modes 'cb-scala-sbt-file-mode)
-    (add-to-list 'aggressive-indent-excluded-modes 'cb-web-js-mode)
-    (add-to-list 'aggressive-indent-excluded-modes 'diff-auto-refine-mode)
-    (add-to-list 'aggressive-indent-excluded-modes 'dockerfile-mode)
-    (add-to-list 'aggressive-indent-excluded-modes 'fstar-mode)
-    (add-to-list 'aggressive-indent-excluded-modes 'graphviz-dot-mode)
-    (add-to-list 'aggressive-indent-excluded-modes 'haskell-mode)
-    (add-to-list 'aggressive-indent-excluded-modes 'idris-mode)
-    (add-to-list 'aggressive-indent-excluded-modes 'idris-repl-mode)
-    (add-to-list 'aggressive-indent-excluded-modes 'restclient-mode)
-    (add-to-list 'aggressive-indent-excluded-modes 'rust-mode)
-    (add-to-list 'aggressive-indent-excluded-modes 'scala-mode)
-    (add-to-list 'aggressive-indent-excluded-modes 'sql-mode)
-    (add-to-list 'aggressive-indent-excluded-modes 'stylus-mode)
-    (add-to-list 'aggressive-indent-excluded-modes 'toml-mode)
+  :init
+  (defconst aggressive-indent-excluded-modes
+    '(cb-scala-sbt-file-mode
+      cb-web-js-mode
+      diff-auto-refine-mode
+      dockerfile-mode
+      fstar-mode
+      graphviz-dot-mode
+      haskell-mode
+      idris-mode
+      idris-repl-mode
+      inf-ruby-mode
+      makefile-gmake-mode
+      makefile-mode
+      nix-mode
+      python-mode
+      restclient-mode
+      rust-mode
+      scala-mode
+      sql-mode
+      stylus-mode
+      text-mode
+      toml-mode
+      yaml-mode))
 
-    (global-aggressive-indent-mode +1)))
+  :config
+  (global-aggressive-indent-mode +1))
 
 (provide 'cb-aggressive-indent)
 
