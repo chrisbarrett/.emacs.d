d144eab07eb2108f3ec75eeeb1bd8d396c515573
Chris Barrett <chris+git@walrus.cool>
2018-02-12 14:49:52 +1300

    Tweak light theme

--- config/cb-faces/cb-light-theme.el
+++ config/cb-faces/cb-light-theme.el
@@ -12,17 +12,20 @@
 
 (require 'cb-theme-common)
 
+(defconst cb-light-theme-blue "#4271ae")
+(defconst cb-light-theme-aqua "#3e999f")
+
 (apply #'custom-theme-set-faces 'cb-light
        (cb-theme-common-make-theme
         :default-fg "black"
         :default-bg "grey90"
         :region-bg "lightblue"
         :subtle-bg "#e1dddd"
-        :paren-match cb-theme-common-cyan
+        :paren-match cb-light-theme-aqua
         :highlight-bg "gray82"
         :header-line-bg "#c6c6c6"
         :header-line-fg "#4d4d4c"
-        :emphasis cb-theme-common-blue
+        :emphasis cb-light-theme-blue
         :pending-bg "#FFFFAA"))
 
 (provide-theme 'cb-light)
