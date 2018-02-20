6b7b50a1b9a8c67641f2d9780ff66c21dd6bb12a
Chris Barrett <chris@walrus.cool>
2018-02-20 19:43:28 +1300

    More icons

--- config/cb-modeline.el
+++ config/cb-modeline.el
@@ -18,9 +18,13 @@
   :init
   (defvar all-the-icons-scale-factor 1.0)
   :config
-  (dolist (spec '((nix-mode all-the-icons-faicon "file-o" :height 0.8 :v-adjust 0.0 :face all-the-icons-dsilver)
+  (dolist (spec '((nix-mode all-the-icons-faicon "linux" :face all-the-icons-purple)
                   (makefile-mode all-the-icons-fileicon "gnu" :face all-the-icons-dorange)
-                  (makefile-bsdmake-mode all-the-icons-fileicon "gnu" :face all-the-icons-dorange)))
+                  (makefile-bsdmake-mode all-the-icons-fileicon "gnu" :face all-the-icons-dorange)
+                  (mu4e-main-mode all-the-icons-octicon "inbox" :face all-the-icons-dsilver)
+                  (mu4e-headers-mode all-the-icons-octicon "inbox" :face all-the-icons-dsilver)
+                  (mu4e-view-mode all-the-icons-octicon "comment-discussion" :face all-the-icons-dsilver)
+                  (mu4e-compose-mode all-the-icons-octicon "comment-discussion" :face all-the-icons-orange)))
     (add-to-list 'all-the-icons-mode-icon-alist spec)))
 
 (use-package cb-header-line-format
