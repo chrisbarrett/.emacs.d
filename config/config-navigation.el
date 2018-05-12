5f14a1d7b8052df9237ca3408f9ee0c7d917480b
Chris Barrett <chris@walrus.cool>
2018-05-13 00:29:19 +1200

    Remove avy. I never use it.

--- config/config-navigation.el
+++ config/config-navigation.el
@@ -13,24 +13,9 @@
 
 (require 'spacemacs-keys)
 
-(spacemacs-keys-declare-prefix "j" "jumping")
 (spacemacs-keys-declare-prefix "x" "urls")
 
 
-
-(use-package avy
-  :straight t
-  :bind (:map
-         spacemacs-keys-default-map
-         ("jb" . avy-pop-mark)
-         ("jj" . evil-avy-goto-char)
-         ("jJ" . evil-avy-goto-char-2)
-         ("jl" . evil-avy-goto-line)
-         ("jw" . evil-avy-goto-word-or-subword-1))
-  :config
-  (progn
-    (setq avy-all-windows 'all-frames)
-    (setq avy-background t)))
 
 (use-package link-hint
   :straight t
