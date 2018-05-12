2a235c487d32684606f9895c3eb661e3928b658d
Chris Barrett <chris@walrus.cool>
2018-05-12 19:04:57 +1200

    Clean up flycheck config

--- config/config-flycheck.el
+++ config/config-flycheck.el
@@ -12,19 +12,41 @@
   (require 'config-etags)
   (require 'use-package))
 
-(autoload 'evil-define-key "evil")
+(autoload 'evil-define-key "evil" nil nil 'macro)
 (autoload 'evil-set-initial-state "evil")
 
+(require 'dash)
+(require 'subr-x)
 (require 'spacemacs-keys)
 
 (use-package flycheck
   :straight t
-  :defer 3
-  :commands (global-flycheck-mode
-             flycheck-list-errors
+  :hook (prog-mode . flycheck-mode-on-safe)
+
+  :commands (flycheck-list-errors
              flycheck-error-list-next-error
              flycheck-error-list-previous-error
              flycheck-error-list-goto-error)
+
+  :bind
+  (:map
+   flycheck-mode-map
+   ("M-n" . flycheck-next-error)
+   ("M-p" . flycheck-previous-error)
+   ("M-j" . flycheck-next-error)
+   ("M-k" . flycheck-previous-error)
+   :map
+   spacemacs-keys-default-map
+   ("ec" . flycheck-clear)
+   ("eh" . flycheck-describe-checker)
+   ("ee" . flycheck-explain-error-at-point)
+   ("en" . flycheck-next-error)
+   ("eN" . flycheck-previous-error)
+   ("ep" . flycheck-previous-error)
+   ("es" . flycheck-select-checker)
+   ("eS" . flycheck-set-checker-executable)
+   ("ev" . flycheck-verify-setup))
+
   :preface
   (progn
     (autoload 'flycheck-buffer "flycheck")
@@ -34,10 +56,19 @@
     (autoload 'projectile-project-p "projectile")
     (autoload 'projectile-process-current-project-buffers "projectile")
 
-    (defun cb-flycheck--inhibit-if-query-replacing (result)
+    (defun config-flycheck-toggle-error-list ()
+      "Show or hide the error list."
+      (interactive)
+      (if-let* ((window (--first (equal flycheck-error-list-buffer
+                                        (buffer-name (window-buffer it)))
+                                 (window-list))))
+          (delete-window window)
+        (flycheck-list-errors)))
+
+    (defun config-flycheck--inhibit-if-query-replacing (result)
       (and result (not config-etags-in-query-replace-session-p)))
 
-    (defun cb-flycheck--check-all-project-buffers ()
+    (defun config-flycheck--check-all-project-buffers ()
       (unless config-etags-in-query-replace-session-p
         (when (and (bound-and-true-p projectile-mode)
                    (projectile-project-p))
@@ -47,32 +78,26 @@
                (when (bound-and-true-p flycheck-mode)
                  (flycheck-buffer))))))))
 
-    (defun cb-flycheck-display-error-messages (errors)
+    (defun config-flycheck-display-error-messages (errors)
       (unless (flycheck-get-error-list-window 'current-frame)
         (when (and errors (flycheck-may-use-echo-area-p))
           (let ((messages (seq-map #'flycheck-error-format-message-and-id errors)))
             (display-message-or-buffer (string-join messages "\n\n")
                                        flycheck-error-message-buffer
-                                       'display-buffer-popup-window)))))
+                                       'display-buffer-popup-window))))))
 
-    (defun cb-flycheck-toggle-error-list ()
-      "Show or hide the error list."
-      (interactive)
-      (if-let (window (--first (equal flycheck-error-list-buffer
-                                      (buffer-name (window-buffer it)))
-                               (window-list)))
-          (delete-window window)
-        (flycheck-list-errors))))
+  :init
+  (spacemacs-keys-set-leader-keys "el" #'config-flycheck-toggle-error-list)
 
   :config
   (progn
     (global-flycheck-mode +1)
 
-    (advice-add #'flycheck-may-enable-mode :filter-return #'cb-flycheck--inhibit-if-query-replacing)
+    (advice-add #'flycheck-may-enable-mode :filter-return #'config-flycheck--inhibit-if-query-replacing)
 
-    (add-hook 'after-save-hook #'cb-flycheck--check-all-project-buffers)
+    (add-hook 'after-save-hook #'config-flycheck--check-all-project-buffers)
 
-    (setq flycheck-display-errors-function 'cb-flycheck-display-error-messages)
+    (setq flycheck-display-errors-function 'config-flycheck-display-error-messages)
     (setq flycheck-display-errors-delay 0.5)
     (setq flycheck-emacs-lisp-load-path 'inherit)
 
@@ -83,18 +108,6 @@
                 ;; restclient buffers
                 js-mode))
 
-    (spacemacs-keys-set-leader-keys
-      "ec" 'flycheck-clear
-      "eh" 'flycheck-describe-checker
-      "el" 'cb-flycheck-toggle-error-list
-      "ee" 'flycheck-explain-error-at-point
-      "en" 'flycheck-next-error
-      "eN" 'flycheck-previous-error
-      "ep" 'flycheck-previous-error
-      "es" 'flycheck-select-checker
-      "eS" 'flycheck-set-checker-executable
-      "ev" 'flycheck-verify-setup)
-
     (with-eval-after-load 'evil
       (evil-set-initial-state 'flycheck-error-list-mode 'motion)
       (evil-define-key 'motion flycheck-error-list-mode-map
@@ -104,11 +117,6 @@
         (kbd "n") #'flycheck-error-list-next-error
         (kbd "p") #'flycheck-error-list-previous-error
         (kbd "q") #'quit-window))
-
-    (bind-key "M-n" 'flycheck-next-error flycheck-mode-map)
-    (bind-key "M-p" 'flycheck-previous-error flycheck-mode-map)
-    (bind-key "M-j" 'flycheck-next-error flycheck-mode-map)
-    (bind-key "M-k" 'flycheck-previous-error flycheck-mode-map)
 
     (add-to-list 'display-buffer-alist
                  `(,(rx bos "*Flycheck errors*" eos)
