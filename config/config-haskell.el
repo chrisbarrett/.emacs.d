f31046cb5fe42e0d6a9365b1056bd97ce7ac0f90
Chris Barrett <chris@walrus.cool>
2018-05-14 20:52:07 +1200

    Major-mode hydra for haskell

--- config/config-haskell.el
+++ config/config-haskell.el
@@ -11,9 +11,25 @@
 (eval-when-compile
   (require 'use-package))
 
-(require 'spacemacs-keys)
+(require 'major-mode-hydra)
 
 (autoload 'evil-define-key "evil")
+
+
+
+(major-mode-hydra-bind haskell-mode "Insert"
+  ("i" haskell-imports-insert-qualified "import")
+  ("u" haskell-imports-insert-unqualified "import (unqualified)")
+  ("p" haskell-pragmas-insert "language pragma"))
+
+(major-mode-hydra-bind haskell-mode "Intero"
+  ("t" intero-targets "set targets"))
+
+(major-mode-hydra-bind haskell-mode "Format"
+  ("f" hindent-reformat-buffer "buffer")
+  ("." hindent-reformat-decl-or-fill "decl at pt"))
+
+
 
 (use-package haskell-mode
   :straight t
@@ -33,12 +49,16 @@
     (add-to-list 'completion-ignored-extensions ".gm"))
 
   :preface
-  (defun cb-haskell--set-indentation-step ()
+  (defun config-haskell--set-indentation-step ()
     (with-no-warnings (setq evil-shift-width 4))
     (setq tab-width 4))
 
   :config
   (progn
+    (setq haskell-compile-cabal-build-command "stack build --ghc-options -ferror-spans")
+    (setq haskell-completing-read-function #'completing-read)
+    (setq haskell-interactive-popup-errors nil)
+
     ;; Use 4 space indentation style.
 
     (setq haskell-indentation-layout-offset 4)
@@ -46,8 +66,7 @@
     (setq haskell-indentation-where-pre-offset 2)
     (setq haskell-indentation-where-post-offset 2)
     (setq haskell-indentation-left-offset 4)
-
-    (add-hook 'haskell-mode-hook #'cb-haskell--set-indentation-step)
+    (add-hook 'haskell-mode-hook #'config-haskell--set-indentation-step)
 
     (add-to-list 'display-buffer-alist
                  `(,(rx bos "*intero:")
@@ -67,37 +86,16 @@
                    (slot            . 1)
                    (window-height   . 0.5)))))
 
-(use-package haskell-customize
-  :after haskell-mode
-  :config
-  (progn
-    ;; (setq haskell-stylish-on-save t)
-    (setq haskell-completing-read-function #'completing-read)
-    (setq haskell-interactive-popup-errors nil)))
-
-(use-package haskell-compile
-  :after haskell-mode
-  :commands (haskell-compile)
-  :config
-  (progn
-    (spacemacs-keys-set-leader-keys-for-major-mode 'haskell-mode
-      "c" #'haskell-compile)
-
-    (setq haskell-compile-cabal-build-command "stack build --ghc-options -ferror-spans")))
-
 (use-package haskell-cabal
   :after haskell-mode
   :config
   (define-key haskell-cabal-mode-map (kbd "C-c C-c") 'haskell-compile))
 
 (use-package haskell-doc
-  :after haskell-mode)
+  :hook (haskell-mode . haskell-doc-mode))
 
 (use-package haskell-interactive-mode
-  :after haskell-mode
-  :commands (interactive-haskell-mode)
-  :init
-  (add-hook 'haskell-mode-hook #'interactive-haskell-mode))
+  :hook (haskell-mode . interactive-haskell-mode))
 
 (use-package haskell-debug
   :after haskell-mode
@@ -117,49 +115,33 @@
   (evil-define-key 'normal haskell-presentation-mode-map (kbd "q") #'quit-window))
 
 (use-package haskell-imports
-  :after haskell-mode
   :commands (haskell-imports-insert-unqualified
-             haskell-imports-insert-qualified)
-  :config
-  (spacemacs-keys-set-leader-keys-for-major-mode 'haskell-mode
-    "ii" #'haskell-imports-insert-unqualified
-    "iq" #'haskell-imports-insert-qualified))
+             haskell-imports-insert-qualified))
 
 (use-package haskell-pragmas
-  :after haskell-mode
-  :commands (haskell-pragmas-insert)
-  :config
-  (spacemacs-keys-set-leader-keys-for-major-mode 'haskell-mode "il" #'haskell-pragmas-insert))
+  :commands (haskell-pragmas-insert))
 
 (use-package intero
   :straight t
   :after haskell-mode
-
-  :commands (intero-mode intero-targets intero-goto-definition)
+  :commands (intero-targets intero-goto-definition)
+  :bind (:map
+         intero-mode-map
+         ("M-." . intero-goto-definition)
+         ("M-," . pop-tag-mark))
 
   :preface
   (progn
     (autoload 'intero-mode-map "intero")
 
-    (defun cb-haskell--maybe-intero-mode ()
+    (defun config-haskell--maybe-intero-mode ()
       (unless (or (derived-mode-p 'ghc-core-mode)
                   (equal (get major-mode 'mode-class) 'special))
         (intero-mode +1))))
-
   :init
-  (add-hook 'haskell-mode-hook #'cb-haskell--maybe-intero-mode)
-
-  :bind
-  (:map
-   intero-mode-map
-   ("M-." . intero-goto-definition)
-   ("M-," . pop-tag-mark))
-
+  (add-hook 'haskell-mode-hook #'config-haskell--maybe-intero-mode)
   :config
   (progn
-    (spacemacs-keys-set-leader-keys-for-major-mode 'haskell-mode
-      "t" #'intero-targets)
-
     (with-eval-after-load 'flycheck
       (flycheck-add-next-checker 'intero 'haskell-hlint))
 
@@ -169,22 +151,12 @@
 
 (use-package hindent
   :straight t
-  :after haskell-mode
-  :commands (hindent-mode
-             hindent-reformat-decl-or-fill
-             hindent-reformat-buffer)
-  :init
-  (spacemacs-keys-set-leader-keys-for-major-mode 'haskell-mode
-    "." #'hindent-reformat-buffer
-    "f" #'hindent-reformat-decl-or-fill)
+  :hook (haskell-mode . hindent-mode)
   :config
-  (progn
-    (setq hindent-reformat-buffer-on-save t)
-    (add-hook 'haskell-mode-hook #'hindent-mode)))
+  (setq hindent-reformat-buffer-on-save t))
 
 (use-package stack-hoogle
-  :after haskell-mode
-  :commands (stack-hoogle stack-hoogle-info-at-pt)
+  :commands (stack-hoogle-info-at-pt)
   :bind
   (:map haskell-mode-map ("C-c C-h" . stack-hoogle))
   :config
