3aba8a6b4391b53d062664c370b98dd1ece15db0
Chris Barrett <chris@walrus.cool>
2018-05-19 12:21:27 +1200

    Entity-decode strings for org link capture

--- lisp/cb-org-capture-url.el
+++ lisp/cb-org-capture-url.el
@@ -1,26 +1,5 @@
 ;;; cb-org-capture-url.el --- Utilities for capturing URLs.  -*- lexical-binding: t; -*-
-
-;; Copyright (C) 2016  Chris Barrett
-
-;; Author: Chris Barrett <chris.d.barrett@me.com>
-
-;; Package-Requires: ((s "1.10.0") (dash "2.12.0") (org "8.3.4"))
-
-;; This program is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 ;;; Commentary:
-
 ;;; Code:
 
 (require 'dash)
@@ -28,13 +7,23 @@
 
 (autoload 'thing-at-point-url-at-point "thingatpt")
 
+(defun cb-org-capture-url--decode-html-entities (str)
+  (with-temp-buffer
+    (insert str)
+    (pcase (libxml-parse-html-region (point-min) (point-max))
+      (`(html nil
+              (body nil
+                    (p nil
+                       ,(and (pred stringp) decoded))))
+       decoded))))
+
 (defun cb-org-capture-url--parse-html-title (html)
   "Extract the title from an HTML document."
   (-let (((_ title) (s-match (rx "<title>" (group (* nonl)) "</title>") html))
          ((_ charset) (-map 'intern (s-match (rx "charset=" (group (+ (any "-" alnum)))) html))))
-    (if (-contains? coding-system-list charset)
-        (decode-coding-string title charset)
-      title)))
+    (cb-org-capture-url--decode-html-entities (if (-contains? coding-system-list charset)
+                                (decode-coding-string title charset)
+                              title))))
 
 (defun cb-org-capture-url--retrieve-html (url)
   "Download the resource at URL and attempt to extract an HTML title."
