3e6d8ceb0f0d99135c78e4450b1158acaadbcd52
Chris Barrett <chris@walrus.cool>
2018-05-26 20:02:14 +1200

    Tweak how recentf items are displayed using counsel-hacks

--- hacks/counsel-hacks.el
+++ hacks/counsel-hacks.el
@@ -15,14 +15,18 @@
 
 (with-eval-after-load 'counsel
   (with-no-warnings
+    (defun counsel-hacks-recentf-candidates ()
+      (seq-uniq (seq-map (-compose #'f-abbrev #'substring-no-properties #'file-truename)
+                         recentf-list)))
+
     (el-patch-defun counsel-recentf ()
       "Find a file on `recentf-list'."
       (interactive)
       (require 'recentf)
       (recentf-mode)
-      (ivy-read "Recentf: " (mapcar (el-patch-swap #'substring-no-properties
-                                                   (-compose #'f-abbrev #'substring-no-properties))
-                                    recentf-list)
+      (ivy-read "Recentf: " (el-patch-swap
+                              (mapcar #'substring-no-properties recentf-list)
+                              (counsel-hacks-recentf-candidates))
                 :action (lambda (f)
                           (with-ivy-window
                             (find-file f)))
