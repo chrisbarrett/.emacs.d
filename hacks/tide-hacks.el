15b14b898ef3c336222b487ab6485f95e5965cdf
Chris Barrett <chris@walrus.cool>
2018-06-27 11:40:23 +1200

    Update tide patch

--- hacks/tide-hacks.el
+++ hacks/tide-hacks.el
@@ -17,11 +17,20 @@
       (tide-command:quickinfo
        (tide-on-response-success-callback response
            (-if-let (buffer (tide-construct-documentation (plist-get response :body)))
-               (el-patch-swap (display-buffer buffer t)
-                              (progn
-                                (with-current-buffer buffer
-                                  (help-mode))
-                                (pop-to-buffer buffer t)))
+               (progn
+                 (el-patch-add (progn
+                                 (with-current-buffer buffer
+                                   (help-mode))
+                                 (pop-to-buffer buffer t)))
+                 (display-buffer buffer t)
+                 (el-patch-swap
+                   (if help-window-select
+                       (progn
+                         (pop-to-buffer buffer)
+                         (message "Type \"q\" to restore previous buffer"))
+                     (message "Type \"q\" in the documentation buffer to close it"))
+                   (when help-window-select
+                     (pop-to-buffer buffer))))
              (message "No documentation available.")))))))
 
 (provide 'tide-hacks)
