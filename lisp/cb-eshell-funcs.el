c8a642704358e929f27e5a1499b4669ad477b10b
Chris Barrett <chris@walrus.cool>
2018-08-18 22:53:14 +1200

    Refactor and fix issues with git repo nav

--- lisp/cb-eshell-funcs.el
+++ lisp/cb-eshell-funcs.el
@@ -3,24 +3,24 @@
 ;;; Code:
 
 (autoload 'magit-status "magit")
+(autoload 'magit-toplevel "magit")
 (autoload 'eshell/cd "em-dirs")
 
 (defun cb-eshell-funcs--fasd-dir (query)
   (let ((result (shell-command-to-string (format "fasd -l -R -d %s" (shell-quote-argument query)))))
-    (car (split-string result "\n" t))))
+    (or (car (split-string result "\n" t))
+        (user-error "No results"))))
 
 (defun eshell/j (&rest query)
   "Change to a directory using fasd with QUERY."
   (unless query
     (user-error "Usage error: must supply a query"))
-  (if-let ((dir (cb-eshell-funcs--fasd-dir (string-join query " "))))
-      (eshell/cd dir)
-    (user-error "No results")))
+  (eshell/cd (cb-eshell-funcs--fasd-dir (string-join query " "))))
 
 (defun eshell/g (&rest query)
   "Open magit, optionally using fasd QUERY to find repo directory."
   (if query
-      (magit-status (cb-eshell-funcs--fasd-dir (string-join query " ")))
+      (magit-status (magit-toplevel (cb-eshell-funcs--fasd-dir (string-join query " "))))
     (magit-status)))
 
 (provide 'cb-eshell-funcs)
