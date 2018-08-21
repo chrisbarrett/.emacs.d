b36f2fe2060511d5b2b0adaed4a1c30d82a8255b
Chris Barrett <chris@walrus.cool>
2018-08-21 16:50:03 +1200

    Remove irony

--- config/config-flycheck.el
+++ config/config-flycheck.el
@@ -134,20 +134,6 @@
     (setq checkdoc-force-docstrings-flag nil)
     (setq checkdoc-arguments-in-order-flag nil)))
 
-;; flycheck-irony provides C/C++/ObjC checking with the Irony server.
-
-(use-package flycheck-irony
-  :straight t
-  :hook (flycheck-mode . flycheck-irony-setup))
-
-(use-package irony
-  :straight t
-  :hook
-  ((c++-mode . irony-mode)
-   (c-mode . irony-mode)
-   (objc-mode . irony-mode)
-   (irony-mode . irony-cdb-autosetup-compile-options)))
-
 (provide 'config-flycheck)
 
 ;;; config-flycheck.el ends here
