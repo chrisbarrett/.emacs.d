b2d47250f003a7fa982d54e021c04d0247bd7f63
Chris Barrett <chris@walrus.cool>
2019-05-16 11:25:16 -0700

    Configure rg and friends

--- config/config-editing.el
+++ config/config-editing.el
@@ -346,6 +346,45 @@ Interactively, reverse the characters in the current region."
   (dolist (form autoinsert-funcs-forms)
     (push form auto-insert-alist)))
 
+;; ag provides integration with the `ag' program.
+
+(use-package ag
+  :straight t
+  :commands ag)
+
+;; rg provides integration with the `ripgrep' program.
+
+(use-package rg
+  :straight t
+  :commands rg
+  :config
+  (setq rg-group-result t))
+
+;; wgrep allows you to edit search results in a buffer then write those changes
+;; to disk.
+
+(use-package wgrep
+  :straight t
+  :commands (wgrep-setup)
+  :general
+  (:keymaps 'wgrep-mode-map [remap wgrep-finish-edit] #'config-editing--wgrep-finish-edit-kill-buffer)
+  :init
+  (add-hook 'grep-setup-hook #'wgrep-setup)
+  :preface
+  (progn
+    (autoload 'wgrep-finish-edit "wgrep")
+
+    (defun config-editing--wgrep-finish-edit-kill-buffer ()
+      "Finish the current wgrep edit and kill the wgrep buffer."
+      (interactive)
+      (let ((buf (current-buffer)))
+        (prog1 (wgrep-finish-edit)
+          (kill-buffer buf)))))
+
+  :config
+  (progn
+    (setq wgrep-enable-key (kbd "C-c C-e"))
+    (setq wgrep-auto-save-buffer t)))
 
 (provide 'config-editing)
 
