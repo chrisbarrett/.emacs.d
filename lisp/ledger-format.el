f9066965f67ccea70ec583238d02bdab02409715
Chris Barrett <chris@walrus.cool>
2019-05-17 13:40:18 -0700

    Add ledger config

---
+++ lisp/ledger-format.el
@@ -0,0 +1,38 @@
+;;; ledger-format.el --- Buffer formatting commands for ledger files.  -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
+
+(require 's)
+
+(autoload 'ledger-mode-clean-buffer "ledger-mode")
+
+(defvar ledger-post-amount-alignment-column 52)
+
+(defun ledger-format--align-price-assertion ()
+  (let ((line (buffer-substring (line-beginning-position) (line-end-position))))
+    (when (s-matches? (rx "=" (* space) (not (any digit)))
+                      line)
+      (unwind-protect
+          (progn
+            (goto-char (line-beginning-position))
+            (search-forward "=")
+            (goto-char (match-beginning 0))
+            (indent-to (1+ ledger-post-amount-alignment-column))
+            (skip-chars-forward " =")
+            (just-one-space))
+        (goto-char (line-end-position))))))
+
+;;;###autoload
+(defun ledger-format-buffer ()
+  "Reformat the buffer."
+  (interactive "*")
+  (let ((pos (point)))
+    (ledger-mode-clean-buffer)
+    (goto-char (point-min))
+    (while (search-forward "=" nil t)
+      (ledger-format--align-price-assertion))
+    (goto-char pos)))
+
+(provide 'ledger-format)
+
+;;; ledger-format.el ends here
