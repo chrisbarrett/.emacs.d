986e992bd9bceca07416e1561e6fb93a1fbc6f09
Chris Barrett <chris@walrus.cool>
2019-05-17 13:11:30 -0700

    Add company

---
+++ config/config-company.el
@@ -0,0 +1,59 @@
+;;; config-company.el --- Configuration for company-mode.  -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
+
+(eval-when-compile
+  (require 'use-package))
+
+(use-package company
+  :straight t
+  :defer t
+  :hook (after-init . global-company-mode)
+
+  :general
+  ([remap completion-at-point] #'company-manual-begin
+   [remap complete-symbol] #'company-manual-begin
+   "S-<return>" #'company-complete
+   :keymaps 'comint-mode-map
+   [remap indent-for-tab-command] #'company-manual-begin)
+
+  :preface
+  (general-unbind :keymaps 'company-active-map "C-w" "C-h")
+
+  :preface
+  (defun config-company--set-company-vars ()
+    ;; HACK: evil-collection-company seems to be messing with this binding, so I
+    ;; need to manually apply it again. :/
+    (define-key company-active-map (kbd "RET") #'company-complete-selection)
+    (setq company-minimum-prefix-length 3)
+    (setq company-tooltip-align-annotations t))
+
+  :config
+  (progn
+    (evil-define-key 'insert company-mode-map (kbd "S-<return>") #'company-complete)
+    (setq company-idle-delay 0.3)
+    (setq company-require-match nil)
+    (add-hook 'company-mode-hook #'config-company--set-company-vars)))
+
+(use-package company-box
+  :straight t
+  :defer t
+  :general
+  (:keymaps '(company-box-mode-map company-active-map company-search-map company-filter-map)
+   "C-n" #'company-box--next-line
+   "C-p" #'company-box--prev-line)
+  :hook (company-mode . company-box-mode)
+  :config
+  (setq company-box-enable-icon nil))
+
+(use-package company-dabbrev
+  :after company
+  :defer t
+  :config
+  (progn
+    (setq company-dabbrev-ignore-case nil)
+    (setq company-dabbrev-downcase nil)))
+
+(provide 'config-company)
+
+;;; config-company.el ends here
---
+++ hacks/company-box-hacks.el
@@ -0,0 +1,51 @@
+;;; company-box-hacks.el --- Hacks for company-box.  -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
+
+(require 'el-patch)
+
+(el-patch-feature company-box)
+
+
+;; Fix background colour on theme change.
+
+(with-eval-after-load 'company-box
+  (el-patch-defun company-box--display (string)
+    "Display the completions."
+    (company-box--render-buffer string)
+    (unless (company-box--get-frame)
+      (company-box--set-frame (company-box--make-frame)))
+    (el-patch-add
+      (with-selected-frame (company-box--get-frame)
+        (set-background-color (face-background 'company-box-background nil t))))
+    (company-box--set-frame-position (company-box--get-frame))
+    (unless (frame-visible-p (company-box--get-frame))
+      (make-frame-visible (company-box--get-frame)))
+    (company-box--update-scrollbar (company-box--get-frame) t)))
+
+
+;; Fix the foreground and background colours of documentation popup frames.
+
+(with-eval-after-load 'company-box-doc
+  (with-no-warnings
+    (el-patch-defun company-box-doc--show (selection frame)
+      (-when-let* ((valid-state (and (eq (selected-frame) frame)
+                                     (company-box--get-frame)
+                                     (frame-visible-p (company-box--get-frame))))
+                   (candidate (nth selection company-candidates))
+                   (doc (or (company-call-backend 'quickhelp-string candidate)
+                            (company-box-doc--fetch-doc-buffer candidate)))
+                   (doc (company-box-doc--make-buffer doc)))
+        (unless (frame-live-p (frame-parameter nil 'company-box-doc-frame))
+          (set-frame-parameter nil 'company-box-doc-frame (company-box-doc--make-frame doc)))
+        (el-patch-add
+          (with-selected-frame (frame-parameter nil 'company-box-doc-frame)
+            (set-foreground-color (face-foreground 'company-box-candidate nil t))
+            (set-background-color (face-background 'company-box-background nil t))))
+        (company-box-doc--set-frame-position (frame-parameter nil 'company-box-doc-frame))
+        (unless (frame-visible-p (frame-parameter nil 'company-box-doc-frame))
+          (make-frame-visible (frame-parameter nil 'company-box-doc-frame)))))))
+
+(provide 'company-box-hacks)
+
+;;; company-box-hacks.el ends here
