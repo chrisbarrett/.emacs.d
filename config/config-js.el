615677480ca1452c7cf5e5e17fb1c032e1fdc414
Chris Barrett <chris@walrus.cool>
2019-05-29 14:27:20 +1200

    Use rjsx-mode

---
+++ config/config-js.el
@@ -0,0 +1,28 @@
+;;; config-js.el --- Configuration for JavaScript editing.  -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
+
+(eval-when-compile
+  (require 'use-package))
+
+
+
+(use-package rjsx-mode
+  :straight t
+  :mode ("\\.jsx?\\'" . rjsx-mode))
+
+(use-package nvm
+  :straight t
+  :hook (js-mode . config-js-maybe-use-nvm)
+  :functions (nvm-use-for-buffer)
+  :custom ((nvm-dir "~/.config/nvm"))
+  :preface
+  (defun config-js-maybe-use-nvm ()
+    (when (locate-dominating-file default-directory ".nvmrc")
+      (nvm-use-for-buffer)
+      t)))
+
+
+(provide 'config-js)
+
+;;; config-js.el ends here
