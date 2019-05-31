a71ca578bc489f7105c6ac4b3f9e220e2ec056c8
Chris Barrett <chris@walrus.cool>
2019-05-31 12:21:18 +1200

    Fix ivy posframe issue with small window widths

--- config/config-ivy.el
+++ config/config-ivy.el
@@ -113,7 +113,6 @@
   :config
   (general-setq ivy-display-function #'ivy-posframe-display-at-frame-center
                 ivy-posframe-border-width 20
-                ivy-posframe-width 120
                 ivy-posframe-hide-minibuffer t
                 ivy-posframe-parameters '((alpha 100 100))))
 
