fb841963f7096dae99d42b7f22ccbf94e7ed6984
Chris Barrett <chris@walrus.cool>
2019-06-07 10:36:02 +1200

    Remove js2 themeing

--- config/config-js.el
+++ config/config-js.el
@@ -19,13 +19,7 @@
       (add-to-list 'lsp-language-id-configuration '(js-mode . "javascript")))
     (general-setq js-indent-level 2
                   js-switch-indent-offset 2
-                  js--prettify-symbols-alist '(("function" . ?ƒ)))
-    (custom-theme-set-faces
-     'user
-     '(js2-external-variable ((t (:inherit font-lock-keyword-face))))
-     '(js2-function-call ((t (:inherit default :foreground nil))))
-     '(js2-object-property-access ((t (:inherit js2-object-property :foreground nil :italic nil))))
-     '(js2-object-property ((t (:italic t :foreground nil)))))))
+                  js--prettify-symbols-alist '(("function" . ?ƒ)))))
 
 (use-package nvm
   :straight t
