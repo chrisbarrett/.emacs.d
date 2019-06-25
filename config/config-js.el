190baa0e54cf9b86970d715d41ff35c8655c5ed7
Chris Barrett <chris@walrus.cool>
2019-06-26 11:24:14 +1200

    Update js configuration

--- config/config-js.el
+++ config/config-js.el
@@ -3,34 +3,43 @@
 ;;; Code:
 
 (eval-when-compile
-  (require 'use-package))
+  (require 'use-package)
+  (require 'lsp-mode))
 
 (require 'general)
 (require 'paths)
 
 
 
+;; `js' is the Emacs built-in javascript mode. However, we actually want to use
+;; the newer version that supports JSX, copied from Emacs master.
+
 (use-package js
   :mode ("\\.jsx?\\'" . js-mode)
   :load-path paths-lisp-directory
+  :custom ((js-indent-level 2)
+           (js-switch-indent-offset 2)
+           (js--prettify-symbols-alist '(("function" . ?ƒ))))
   :config
-  (progn
-    (with-eval-after-load 'lsp-mode
-      (add-to-list 'lsp-language-id-configuration '(js-mode . "javascript")))
-    (general-setq js-indent-level 2
-                  js-switch-indent-offset 2
-                  js--prettify-symbols-alist '(("function" . ?ƒ)))))
+  (with-eval-after-load 'lsp-mode
+    (add-to-list 'lsp-language-id-configuration '(js-mode . "javascript"))))
+
+(use-package typescript-mode
+  :straight t
+  :custom ((typescript-indent-level 2)))
 
 (use-package nvm
   :straight t
-  :hook (js-mode . config-js-maybe-use-nvm)
-  :functions (nvm-use-for-buffer)
   :custom ((nvm-dir "~/.config/nvm"))
+
+  :functions (nvm-use-for-buffer)
   :preface
   (defun config-js-maybe-use-nvm ()
     (when (locate-dominating-file default-directory ".nvmrc")
       (nvm-use-for-buffer)
-      t)))
+      t))
+  :hook ((js-mode . config-js-maybe-use-nvm)
+         (typescript-mode . config-js-maybe-use-nvm)))
 
 
 (provide 'config-js)
