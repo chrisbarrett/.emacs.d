eb2225e236df71be755351b7bc5a9d362967e35a
Chris Barrett <chris@walrus.cool>
2019-06-25 13:27:51 +1200

    lisp/ox-slack.el

---
+++ lisp/ox-slack.el
@@ -0,0 +1,69 @@
+;;; ox-slack.el --- Export backend for Rumble Cell Updates  -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
+
+(require 'ox)
+(require 'ox-md)
+
+(autoload 'gfm-mode "markdown-mode")
+
+(defun ox-slack-markup-headline (headline contents info)
+  (let* ((text (org-export-data (org-element-property :title headline) info))
+         (todo (org-export-data (org-element-property :todo-keyword headline) info))
+         (todo-text (unless (or (not (plist-get info :with-todo-keywords))
+                                (string= todo ""))
+                      todo)))
+    (concat
+     (when todo-text (concat todo-text " "))
+     "*" text "*"
+     "\n\n" (when (org-string-nw-p contents) contents))))
+
+(defun ox-slack-italic (_italic contents _info)
+  (format "_%s_" contents))
+
+(defun ox-slack-bold (_bold contents _info)
+  (format "*%s*" contents))
+
+(defun ox-slack-item (item contents info)
+  (let* ((type (org-element-property :type (org-export-get-parent item)))
+         (struct (org-element-property :structure item))
+         (bullet (if (not (eq type 'ordered)) "-"
+                   (concat (number-to-string
+                            (car (last (org-list-get-item-number
+                                        (org-element-property :begin item)
+                                        struct
+                                        (org-list-prevs-alist struct)
+                                        (org-list-parents-alist struct)))))
+                           "."))))
+    (concat bullet
+            " "
+            (pcase (org-element-property :checkbox item)
+              (`on "[X] ")
+              (`trans "[-] ")
+              (`off "[ ] "))
+            (let ((tag (org-element-property :tag item)))
+              (and tag (format "*%s:* "(org-export-data tag info))))
+            (and contents
+                 (org-trim (replace-regexp-in-string "^" "    " contents))))))
+
+(org-export-define-derived-backend 'slack 'md
+  :translate-alist '((headline . ox-slack-markup-headline)
+                     (item . ox-slack-item)
+                     (italic . ox-slack-markup-italic)
+                     (bold . ox-slack-markup-bold))
+  :menu-entry
+  '(?s "Export to Slack Markup"
+       ((?s "To temporary buffer"
+            (lambda (a s v b) (ox-slack-export-to-buffer a s v))))))
+
+(defun ox-slack-export-to-buffer (&optional async subtreep visible-only)
+  "Export the buffer to Slack markup.
+
+ASYNC, SUBTREEP and VISIBLE-ONLY are as specified in the export dispatcher."
+  (interactive)
+  (org-export-to-buffer 'slack "*Org Slack Export*"
+    async subtreep visible-only nil nil (lambda () (gfm-mode))))
+
+(provide 'ox-slack)
+
+;;; ox-slack.el ends here
