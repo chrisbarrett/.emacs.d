f376dab7b402914e592d86935b7d8eede0802e07
Chris Barrett <chris@walrus.cool>
2019-07-16 09:25:33 +1200

    Move treemacs major-mode hydra def into use-package

--- config/config-treemacs.el
+++ config/config-treemacs.el
@@ -7,23 +7,6 @@
 
 (require 'dash)
 (require 'general)
-(require 'major-mode-hydra)
-
-
-
-(major-mode-hydra-bind treemacs-mode "View"
-  ("." treemacs-toggle-show-dotfiles "hidden files"))
-
-(major-mode-hydra-bind treemacs-mode "File"
-  ("fr" treemacs-rename "rename...")
-  ("fx" treemacs-delete "delete..."))
-
-(major-mode-hydra-bind treemacs-mode "Projects"
-  ("pa" treemacs-add-project-to-workspace "add...")
-  ("pr" treemacs-rename-project "rename...")
-  ("px" treemacs-remove-project-from-workspace "remove..."))
-
-
 
 (use-package treemacs
   :straight (:host github :repo "Alexander-Miller/treemacs" :files ("src/elisp/*.el" "src/extra/*.el" "src/scripts/*.py"))
@@ -31,6 +14,18 @@
              treemacs-follow-mode
              treemacs-git-mode
              treemacs-filewatch-mode)
+  :mode-hydra
+  (treemacs-mode
+   ("View"
+    (("." treemacs-toggle-show-dotfiles "hidden files"))
+    "File"
+    (("fr" treemacs-rename "rename...")
+     ("fx" treemacs-delete "delete..."))
+    "Projects"
+    (("pa" treemacs-add-project-to-workspace "add...")
+     ("pr" treemacs-rename-project "rename...")
+     ("px" treemacs-remove-project-from-workspace "remove..."))))
+
   :preface
   (defun config-treemacs-format-icon (item)
     (-let* (((pattern f . spec) item)
