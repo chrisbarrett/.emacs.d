c9cc8babf64ec534dd375b298ca57ccd5c9a953e
Chris Barrett <chris@walrus.cool>
2019-09-24 14:17:42 +1200

    Set the background colour for helm in posframe

---
+++ hacks/helm-posframe-hacks.el
@@ -0,0 +1,30 @@
+;;; helm-posframe-hacks.el --- Hacks for helm-posframe.  -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
+
+(require 'el-patch)
+
+(with-eval-after-load 'helm-posframe
+  (with-no-warnings
+    (el-patch-defun helm-posframe-display (buffer &optional _resume)
+      "The display function which is used by `helm-display-function'.
+Argument BUFFER."
+      (setq helm-posframe-buffer buffer)
+      (posframe-show
+       buffer
+       :position (point)
+       :poshandler helm-posframe-poshandler
+       :width (or helm-posframe-width (+ (window-width) 2))
+       :height (or helm-posframe-height helm-display-buffer-height)
+       :min-height 10
+       :min-width 50
+       :font helm-posframe-font
+       (el-patch-add
+         :background-color (face-background 'ivy-posframe nil t))
+       :override-parameters helm-posframe-parameters
+       :respect-header-line t))))
+
+
+(provide 'helm-posframe-hacks)
+
+;;; helm-posframe-hacks.el ends here
