f73809bd8c19ed8b3c3ff269d47dc3b8fe84e9a2
Chris Barrett <chris@walrus.cool>
2019-12-25 09:03:17 +1300

    Add extends prop to ledger face

--- config/config-themes.el
+++ config/config-themes.el
@@ -435,7 +435,12 @@ By default, this shows the information specified by `global-mode-string'."
         ;; Update some common faces to use this.
         (when (>= emacs-major-version 27)
           (dolist (face (face-list))
-            (when (or (memq face '(org-quote org-block markdown-code-face mu4e-header-highlight-face org-agenda-clocking))
+            (when (or (memq face '(org-quote
+                                   org-block
+                                   markdown-code-face
+                                   mu4e-header-highlight-face
+                                   org-agenda-clocking
+                                   ledger-font-xact-highlight-face))
                       (string-match-p (rx (or "region" "magit" "ediff" "diff" "highlight" "selection"))
                                       (symbol-name face)))
               (set-face-attribute face nil :extend t)))))))
