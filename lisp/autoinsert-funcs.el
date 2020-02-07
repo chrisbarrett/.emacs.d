773edba02a9c2aef8c436a6cf554df8f8f19aa15
Chris Barrett <chris@walrus.cool>
2020-02-07 17:32:31 +1300

    Migrate skeleton to template file

--- lisp/autoinsert-funcs.el
+++ lisp/autoinsert-funcs.el
@@ -9,41 +9,6 @@
 (autoload 'yas-expand-snippet "yasnippet")
 
 
-;;; Elisp
-
-(defconst autoinsert-funcs-el-gnu-license "
-\;; This program is free software; you can redistribute it and/or modify
-\;; it under the terms of the GNU General Public License as published by
-\;; the Free Software Foundation, either version 3 of the License, or
-\;; (at your option) any later version.
-
-\;; This program is distributed in the hope that it will be useful,
-\;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-\;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-\;; GNU General Public License for more details.
-
-\;; You should have received a copy of the GNU General Public License
-\;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
-")
-
-(defconst autoinsert-funcs-el-form
-  '((emacs-lisp-mode . "Emacs Lisp")
-    nil
-    "\;;; " (file-name-nondirectory (buffer-file-name)) " --- <enter description here>  "
-    "-*- lexical-binding: t; -*-" '(setq lexical-binding t) \n
-    \n
-    ";; Copyright (C) " (format-time-string "%Y") "  "
-    (getenv "ORGANIZATION") | user-full-name                \n
-    \n
-    ";; Author: " user-full-name " <" user-mail-address ">" \n
-    autoinsert-funcs-el-gnu-license                            \n
-    ";;; Commentary:"                                       \n \n
-    ";;; Code:"                                             \n \n
-    _                                                       \n \n
-    "\(provide '" (file-name-base) ")"                      \n \n
-    "\;;; " (file-name-nondirectory (buffer-file-name)) " ends here" \n))
-
-
 ;;; Shell-scripts
 
 (defun autoinsert-funcs--sh-template-string ()
@@ -125,7 +90,6 @@ $0
 
 (defconst autoinsert-funcs-forms
   (list autoinsert-funcs-sh-form
-        autoinsert-funcs-el-form
         autoinsert-funcs-hs-src-form
         autoinsert-funcs-hs-test-form
         autoinsert-funcs-html-form))
