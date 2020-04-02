cf19927f08591bbb398fb693f9df72dd9f9ae3e8
Chris Barrett <chris@walrus.cool>
2020-04-02 20:52:48 +1300

    Reference top-level packages so overrides work

--- packages.nix
+++ packages.nix
@@ -3,7 +3,7 @@
   # Set the packages to bake in to emacs. The versions of packages are
   # determined by `emacs-overlay`, but can be overridden in the `overrides`
   # binding later in this file.
-  packages = epkgs: with epkgs.elpaPackages; with epkgs.melpaPackages;
+  packages = epkgs: with epkgs;
   let
     fromOverlay = [
       aggressive-indent
