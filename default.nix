dd237333eb8a357ef3ea9cacaa61e2d912470795
Chris Barrett <chris@walrus.cool>
2020-04-03 15:18:25 +1300

    Remove redundant inherits

--- default.nix
+++ default.nix
@@ -33,8 +33,7 @@ let
 
     github = { name, repo ? name, rev, owner, sha256, buildInputs ? [], patches ? [] }:
       pkgs.callPackage ./builder.nix {
-        inherit emacs;
-        inherit name buildInputs patches;
+        inherit emacs name buildInputs patches;
         src = pkgs.fetchFromGitHub {
           inherit sha256 repo rev owner;
         };
