5b41891fd07e095c075cdf4d7aa958fcb45bde8a
Chris Barrett <chris@walrus.cool>
2020-04-25 21:55:20 +1200

    Move better indent function into elisp config

--- config/config-elisp.el
+++ config/config-elisp.el
@@ -18,6 +18,65 @@
 
 
 
+;; lisp-mode provides definitions of lisp editing modes.
+
+(use-package lisp-mode
+  :preface
+  (progn
+    (defvar calculate-lisp-indent-last-sexp)
+
+    (defun config-elisp--better-lisp-indent-function (indent-point state)
+      (let ((normal-indent (current-column))
+            (orig-point (point)))
+        (goto-char (1+ (elt state 1)))
+        (parse-partial-sexp (point) calculate-lisp-indent-last-sexp 0 t)
+        (cond
+         ;; car of form doesn't seem to be a symbol, or is a keyword
+         ((and (elt state 2)
+               (or (not (looking-at "\\sw\\|\\s_"))
+                   (looking-at ":")))
+          (unless (> (save-excursion (forward-line 1) (point))
+                     calculate-lisp-indent-last-sexp)
+            (goto-char calculate-lisp-indent-last-sexp)
+            (beginning-of-line)
+            (parse-partial-sexp (point) calculate-lisp-indent-last-sexp 0 t))
+
+          ;; Indent under the list or under the first sexp on the same
+          ;; line as calculate-lisp-indent-last-sexp.  Note that first
+          ;; thing on that line has to be complete sexp since we are
+          ;; inside the innermost containing sexp.
+          (backward-prefix-chars)
+          (current-column))
+         ((and (save-excursion
+                 (goto-char indent-point)
+                 (skip-syntax-forward " ")
+                 (not (looking-at ":")))
+               (save-excursion
+                 (goto-char orig-point)
+                 (looking-at ":")))
+          (save-excursion
+            (goto-char (+ 2 (elt state 1)))
+            (current-column)))
+         (t
+          (let ((function (buffer-substring (point)
+                                            (progn (forward-sexp 1) (point))))
+                method)
+            (setq method (or (function-get (intern-soft function)
+                                           'lisp-indent-function)
+                             (get (intern-soft function) 'lisp-indent-hook)))
+            (cond ((or (eq method 'defun)
+                       (and (null method)
+                            (> (length function) 3)
+                            (string-match "\\`def" function)))
+                   (lisp-indent-defform state indent-point))
+                  ((integerp method)
+                   (lisp-indent-specform method state
+                                         indent-point normal-indent))
+                  (method
+                   (funcall method indent-point state)))))))))
+  :custom
+  ((lisp-indent-function #'config-elisp--better-lisp-indent-function)))
+
 ;; simple.el is the basic text-editor command package.
 
 (use-package simple
