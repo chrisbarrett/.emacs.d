72a5e2533bf87cc2491044eca79774a9ecbf1e8f
Chris Barrett <chris@walrus.cool>
2020-07-01 18:31:16 +1200

    nixfmt

--- language-servers/default.nix
+++ language-servers/default.nix
@@ -1,15 +1,15 @@
 { pkgs, ... }:
 let
-  node = pkgs.callPackage ./node {};
+  node = pkgs.callPackage ./node { };
 
   emmyLuaJar = pkgs.fetchurl rec {
     name = "emmy-lua.jar";
-    url = "https://ci.appveyor.com/api/buildjobs/sq7l4h55stcyt4hy/artifacts/EmmyLua-LS%2Fbuild%2Flibs%2FEmmyLua-LS-all.jar";
+    url =
+      "https://ci.appveyor.com/api/buildjobs/sq7l4h55stcyt4hy/artifacts/EmmyLua-LS%2Fbuild%2Flibs%2FEmmyLua-LS-all.jar";
     sha256 = "0pxnbrfb6n3y6a82c41f2ldnpb2r0b18z5d6c0azril5zfwjrk6l";
   };
 
-in
-pkgs.symlinkJoin {
+in pkgs.symlinkJoin {
   name = "language-servers";
   paths = [
     node.bash-language-server
