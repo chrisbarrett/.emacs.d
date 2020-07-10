7bb682abf0326138831db8823db3562eed8f5707
Chris Barrett <chris@walrus.cool>
2020-07-11 01:03:30 +1200

    Add blank lines before new headings

--- config/config-org.el
+++ config/config-org.el
@@ -88,7 +88,7 @@
 (general-setq
  org-imenu-depth 4
  org-M-RET-may-split-line nil
- org-blank-before-new-entry '((heading . nil) (plain-list-item . nil))
+ org-blank-before-new-entry '((heading . t) (plain-list-item . nil))
  org-catch-invisible-edits 'smart
  org-checkbox-hierarchical-statistics t
  org-confirm-elisp-link-function nil
