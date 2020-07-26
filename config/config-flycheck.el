1775e9268882a4e1537f89ee358539b90ee7ccc1
Chris Barrett <chris@walrus.cool>
2020-07-26 16:47:57 +1200

    Handle error in flycheck predicate

--- config/config-flycheck.el
+++ config/config-flycheck.el
@@ -119,7 +119,8 @@
 (defun config-flycheck--elisp-package-flycheck-predicate ()
   (not (or (bound-and-true-p no-byte-compile)
            (bound-and-true-p org-src-mode)
-           (string-match-p "^test-" (file-name-nondirectory (buffer-file-name))))))
+           (ignore-errors
+             (string-match-p "^test-" (file-name-nondirectory (buffer-file-name)))))))
 
 ;; flycheck-package is a linter for Elisp package conventions.
 
