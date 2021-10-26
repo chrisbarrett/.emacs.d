6515ee84e76f404c7b8c9bb013293fefc3859a7e
Chris Barrett <chris@walrus.cool>
2021-10-26 19:02:59 +1300

    Extract node packages out to a dedicated json file

--- language-servers/node/generate.sh
+++ language-servers/node/generate.sh
@@ -1,23 +1,9 @@
 #!/usr/bin/env bash
 set -e -o pipefail
 
-PACKAGE_ARRAY='[
-  "bash-language-server",
-  "dockerfile-language-server-nodejs",
-  "eslint",
-  "flow-bin",
-  "graphql-language-service-cli",
-  "typescript",
-  "typescript-language-server",
-  "vscode-css-languageserver-bin",
-  "vscode-html-languageserver-bin",
-  "vscode-json-languageserver",
-  "yaml-language-server"
-]'
-
-NODE2NIX=$(nix-build '<nixpkgs>' --no-out-link -A 'nodePackages.node2nix')/bin/node2nix
-
 # shellcheck disable=SC2039
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$DIR"
-"$NODE2NIX" -i <(echo "$PACKAGE_ARRAY") --nodejs-14
+
+NODE2NIX=$(nix-build '<nixpkgs>' --no-out-link -A 'nodePackages.node2nix')/bin/node2nix
+"$NODE2NIX" -i node-packages.json --nodejs-14
