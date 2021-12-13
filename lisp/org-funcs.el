0d3188622516207e5f8770c09185b55cd38b3f18
Chris Barrett <chris@walrus.cool>
2021-12-13 15:27:31 +1300

    Automatically format org files on save

--- lisp/org-funcs.el
+++ lisp/org-funcs.el
@@ -356,6 +356,43 @@ With optional prefix ARG, prompt for a tags filter."
              (not (seq-contains-p (org-roam-node-tags node) "dailies"))))))
     (org-roam-node-find arg nil filter)))
 
+
+
+(defun org-funcs-fix-blank-lines (&optional prefix)
+  "Ensure that blank lines exist between headings and their contents.
+With prefix, operate on whole buffer. Ensures that blank lines
+exist after each headings's drawers."
+  (interactive "P")
+  (org-map-entries (lambda ()
+                     (org-with-wide-buffer
+                      ;; `org-map-entries' narrows the buffer, which prevents us from seeing
+                      ;; newlines before the current heading, so we do this part widened.
+                      (while (not (looking-back "\n\n" nil))
+                        ;; Insert blank lines before heading.
+                        (insert "\n")))
+                     (let ((end (org-entry-end-position)))
+                       ;; Insert blank lines before entry content
+                       (forward-line)
+                       (while (and (org-at-planning-p)
+                                   (< (point) (point-max)))
+                         ;; Skip planning lines
+                         (forward-line))
+                       (while (re-search-forward org-drawer-regexp end t)
+                         ;; Skip drawers. You might think that `org-at-drawer-p' would suffice, but
+                         ;; for some reason it doesn't work correctly when operating on hidden text.
+                         ;; This works, taken from `org-agenda-get-some-entry-text'.
+                         (re-search-forward "^[ \t]*:END:.*\n?" end t)
+                         (goto-char (match-end 0)))
+                       (delete-blank-lines)
+                       (when (and (eobp) (not (thing-at-point-looking-at "\n")))
+                         (insert "\n"))))
+                   t (if prefix
+                         nil
+                       'tree)))
+
+(defun org-funcs-format-before-save ()
+  (org-funcs-fix-blank-lines t))
+
 (provide 'org-funcs)
 
 ;;; org-funcs.el ends here
