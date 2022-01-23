d9c3160568c52b1a565be572c17252148b92b25e
Chris Barrett <chris@walrus.cool>
2022-01-24 00:01:24 +1300

    Only cache org files in org-roam-directory

--- lisp/org-roam-review.el
+++ lisp/org-roam-review.el
@@ -236,15 +236,16 @@ https://github.com/org-roam/org-roam/issues/2032"
 (defun org-roam-review--cache-roam-files ()
   (f-files org-roam-directory
            (lambda (file)
-             (with-temp-buffer
-               (insert-file-contents file)
-               (setq-local major-mode 'org-mode)
-               (org-set-regexps-and-options)
-               (unless (org-roam-review--cache-skip-note-p file)
-                 (org-roam-review--cache-mutate (lambda (cache)
-                                                  (org-roam-review--update-by-props-in-buffer cache
-                                                                                              (current-buffer)
-                                                                                              file))))))
+             (when (f-ext-p file "org")
+               (with-temp-buffer
+                 (insert-file-contents file)
+                 (setq-local major-mode 'org-mode)
+                 (org-set-regexps-and-options)
+                 (unless (org-roam-review--cache-skip-note-p file)
+                   (org-roam-review--cache-mutate (lambda (cache)
+                                                    (org-roam-review--update-by-props-in-buffer cache
+                                                                                                (current-buffer)
+                                                                                                file)))))))
            t))
 
 ;;;###autoload
