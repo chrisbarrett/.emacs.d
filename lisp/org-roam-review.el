6c7c50532a23a818980d0e7c9154e7c5b8b2f2af
Chris Barrett <chris@walrus.cool>
2022-02-18 16:10:34 +1300

    Use org-roam's built-in preview functionality

--- lisp/org-roam-review.el
+++ lisp/org-roam-review.el
@@ -185,43 +185,11 @@ nodes for review."
   (replace-regexp-in-string (rx bol) (make-string (* depth org-roam-review-indent-width) 32)
                             str))
 
-(plist-define org-roam-review-preview
-  :required (:content)
-  :optional (:start :heading))
-
-(defun org-roam-review--format-preview (node)
-  (let (content heading start)
-    (with-temp-buffer
-      (let ((org-inhibit-startup t))
-        (org-mode))
-      (insert-file-contents (org-roam-node-file node))
-      (when-let ((pos (org-roam-node-point node)))
-        (goto-char pos))
-      (org-roam-end-of-meta-data t)
-      (when (org-at-heading-p)
-        (forward-line 1))
-      (setq start (line-beginning-position))
-      (setq heading
-            (when-let* ((path (ignore-errors (org-get-outline-path t))))
-              (->> path
-                   (seq-map (lambda (it) (propertize it 'font-lock-face 'org-roam-review-heading)))
-                   (-interpose (propertize " > " 'font-lock-face 'org-roam-review-heading-separator))
-                   (apply 'concat)
-                   (s-append "\n"))))
-      (org-next-visible-heading 1)
-      (setq content (org-roam-fontify-like-in-org-mode
-                     (string-trim (buffer-substring-no-properties start (point))))))
-    (org-roam-review-preview-create
-     :content content
-     :start start
-     :heading heading)))
-
 (defun org-roam-review-insert-preview (node &optional hidden-p depth)
-  (-let ((depth (or depth 0))
-         ((&plist :content :heading :start) (org-roam-review--format-preview node)))
+  (let* ((depth (or depth 0))
+         (start (org-roam-node-point node))
+         (content (org-roam-fontify-like-in-org-mode (org-roam-preview-get-contents (org-roam-node-file node) start))))
     (magit-insert-section section (org-roam-preview-section nil hidden-p)
-      (when heading
-        (magit-insert-heading (org-roam-review-indent-string heading depth)))
       (insert (org-roam-review-indent-string (if (string-blank-p (string-trim-left content))
                                                  (propertize "(Empty)" 'font-lock-face 'font-lock-comment-face)
                                                content)
