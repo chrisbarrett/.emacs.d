3ddf7435af53bf87decad2208eb3f1b045324ead
Chris Barrett <chris@walrus.cool>
2022-09-23 00:19:57 +1200

    Automatically re-tangle and load if lisp config changes

--- init.el
+++ init.el
@@ -77,27 +77,31 @@
 (defun tangle-init-files ()
   "Create init files from `config.org'."
   (interactive)
-  (let ((file-to-block-list))
+  (let* ((file-to-block-list)
+         (inhibit-message noninteractive)
+         (config-org (expand-file-name "config.org" user-emacs-directory))
+         (marker (set-marker (make-marker)
+                             0
+                             (or (find-buffer-visiting config-org)
+                                 (find-file-noselect config-org)))))
+    (require 'org)
     (message "Re-generating init files…")
-    (save-restriction
-      (widen)
-      (save-excursion
-        (require 'org)
-        (let ((inhibit-message t))
-          (org-babel-map-src-blocks "config.org"
-            (when (equal lang "emacs-lisp")
-              (cond ((member "disabled" (org-get-tags))
+    (org-with-point-at marker
+      (let ((inhibit-message t))
+        (org-babel-map-src-blocks "config.org"
+          (when (equal lang "emacs-lisp")
+            (cond ((member "disabled" (org-get-tags))
+                   (when tangle-debug
+                     (message "Init files: Skipping:\n%s" body)))
+                  ((member (org-get-todo-state) '("CANCELLED" "TODO"))
+                   (when tangle-debug
+                     (message "Init files: Skipping todo:\n%s" body)))
+                  (t
+                   (let* ((block-info (org-babel-get-src-block-info t))
+                          (output-file (cdr (assq :tangle (nth 2 block-info)))))
+                     (push (cons output-file body) file-to-block-list)
                      (when tangle-debug
-                       (message "Init files: Skipping:\n%s" body)))
-                    ((member (org-get-todo-state) '("CANCELLED" "TODO"))
-                     (when tangle-debug
-                       (message "Init files: Skipping todo:\n%s" body)))
-                    (t
-                     (let* ((block-info (org-babel-get-src-block-info t))
-                            (output-file (cdr (assq :tangle (nth 2 block-info)))))
-                       (push (cons output-file body) file-to-block-list)
-                       (when tangle-debug
-                         (message "Init files: Adding:\n%s" body)))))))))
+                       (message "Init files: Adding:\n%s" body))))))))
       (let ((lines-by-file
              (thread-last file-to-block-list
                           (seq-group-by #'car)
@@ -112,19 +116,35 @@
               (indent-rigidly (point-min) (point-max) (- org-edit-src-content-indentation))
               (message "Writing %s…" file)))))
 
+      ;; for GC
+      (set-marker marker nil)
       (message "Wrote all init files."))))
 
-(let ((this-file (or load-file-name (buffer-file-name))))
+(defun init-tangle-and-reload ()
+  "Tangle config.org and reload config if changed."
+  (interactive)
+  (let ((config-el (expand-file-name "config.el" user-emacs-directory)))
 
-  (let ((config-autoloads-file (expand-file-name "config-autoloads.el" (file-name-directory this-file))))
+    (cl-labels ((fast-hash ()
+                           (->> (format "cksum %s" (shell-quote-argument config-el))
+                                (shell-command-to-string)
+                                (string-trim))))
+      (let ((hash-before (fast-hash))
+            (noninteractive t))
+        (tangle-init-files)
+        (unless (equal hash-before (fast-hash))
+          (load config-el)
+          (message "Reloaded config.el"))))))
+
+(let ((default-directory user-emacs-directory))
+
+  (let ((config-autoloads-file (expand-file-name "config-autoloads.el")))
     (unless (file-exists-p config-autoloads-file)
-      (let ((default-directory (file-name-directory this-file)))
-        (generate-package-autoloads))))
+      (generate-package-autoloads)))
 
-  (let ((config-lisp-file (expand-file-name "config.el" (file-name-directory this-file))))
+  (let ((config-lisp-file (expand-file-name "config.el")))
     (unless (file-exists-p config-lisp-file)
-      (let ((default-directory (file-name-directory this-file)))
-        (tangle-init-files)))
+      (tangle-init-files))
 
     (unless (getenv "NIX_EMACS_BUILDING_CONFIG_P")
       (load config-lisp-file nil t))))
