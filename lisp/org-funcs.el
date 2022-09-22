ee6935df2b0611baef2cc4678fb71dbbf7930133
Chris Barrett <chris@walrus.cool>
2022-09-22 17:08:34 +1200

    changes from octopus.local on Thu Sep 22 17:08:34 NZST 2022

--- lisp/org-funcs.el
+++ lisp/org-funcs.el
@@ -358,7 +358,7 @@ citar."
       (let ((file (org-roam-node-file node)))
         (setq dest (f-join org-roam-directory slipbox (f-filename file)))
         (call-process vc-git-program nil nil nil "add" file)
-        (vc-update)
+        (vc-refresh-state)
         (vc-rename-file file dest)
         (org-roam-db-sync)))
      (t
