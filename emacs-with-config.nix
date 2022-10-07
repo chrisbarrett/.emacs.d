be9e659a8a0988f18cb724111d35b2ccb7333758
Chris Barrett <chris@walrus.cool>
2022-10-07 19:51:47 +1300

    Revert "Let lsp install language servers itself"
    This reverts commit f8e854c411c1a24109de9667f40c9f463ca9d79b.

--- emacs-with-config.nix
+++ emacs-with-config.nix
@@ -19,10 +19,25 @@ let
     ];
   };
 
+  languageServers = with pkgs; {
+    bash = nodePackages.bash-language-server;
+    css = nodePackages.vscode-css-languageserver-bin;
+    eslint = vscode-extensions.dbaeumer.vscode-eslint;
+    graphql = vscode-extensions.graphql.vscode-graphql;
+    html = nodePackages.vscode-html-languageserver-bin;
+    json = nodePackages.vscode-json-languageserver;
+    typescript = nodePackages.typescript-language-server;
+    yaml = nodePackages.yaml-language-server;
+  };
+
   builders = pkgs.callPackage ./builders { };
   emacsEnv = (pkgs.emacsPackagesFor emacs).overrideScope' (pkgs.callPackage ./pkg-overrides.nix builders);
   packages = import ./packages.nix;
-  customPathEntries = "${requiredPrograms}/bin";
+
+  customPathEntries =
+    let paths = [ "${requiredPrograms}/bin" ] ++ (map (pkg: "${pkg}/bin") (attrsets.attrValues languageServers));
+    in
+    strings.concatStringsSep ":" paths;
 in
 pkgs.symlinkJoin
 {
@@ -35,6 +50,10 @@ pkgs.symlinkJoin
         wrapProgram "$program" \
           --prefix PATH ":" "${customPathEntries}" \
           --set NIX_EMACS_DARWIN_PATH_EXTRAS "${customPathEntries}:/opt/homebrew/bin" \
+          --set NIX_EMACS_ESLINT_SERVER_SCRIPT "${languageServers.eslint}/lib/eslintServer.js" \
+          --set NIX_EMACS_LSP_ESLINT_NODE_PATH "${pkgs.nodejs}/bin/node" \
+          --set NIX_EMACS_TS_LANGUAGE_SERVER "${languageServers.typescript}/bin/typescript-language-server" \
+          --set NIX_EMACS_JSON_LANGUAGE_SERVER "${languageServers.json}/bin/vscode-json-languageserver" \
           --set NIX_EMACS_MU_BINARY "${pkgs.mu}/bin/mu" \
           --set NIX_EMACS_MU_LISP_DIR "${pkgs.mu}/share/emacs/site-lisp/mu4e" \
           --set NIX_EMACS_SRC_DIR "${emacs}/share/emacs" \
