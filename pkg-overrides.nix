b5f472ecf796802cdfe84fb44f1148061b116fdb
Chris Barrett <chris@walrus.cool>
2023-04-07 20:49:12 +1200

    Remove unneeded markdown-mode dep for chatgpt-shell

--- pkg-overrides.nix
+++ pkg-overrides.nix
@@ -4,7 +4,7 @@
     owner = "xenodium";
     rev = "a6f0e1d98012a112ab2557a83197f1b1f34496ca";
     sha256 = "sha256-/9o50mPWIs5rrGjNYxuLDLsYZokFyo2o5nSfI0ByJZE=";
-    buildInputs = [ esuper.markdown-mode esuper.org ];
+    buildInputs = [ esuper.org ];
   };
 
   eglot-x = github {
