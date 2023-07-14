fa50a5e9a12ac28c397bfa5c6c08d411163666e1
Chris Barrett <chris@walrus.cool>
2023-07-15 10:51:26 +1200

    Customise completion-ignored-extensions

--- config/cb-completion.el
+++ config/cb-completion.el
@@ -29,6 +29,22 @@
 ;; Perform both indentation & text completion with TAB.
 (setq tab-always-indent 'complete)
 
+;; Emacs by default ignores a ton of stuff, but most of the entries don't matter
+;; to me.
+(setq completion-ignored-extensions
+      '(
+        ;; VC
+        ".git/"
+        ;; OS-generated
+        ".DS_Store"
+        ".o" "~" ".bin" ".lbin" ".so" ".a"
+        ;; Emacs generated
+        ".elc" ".eln"
+        ;; Nix, direnv, etc
+        ".drv"
+        ".direnv/"
+        ))
+
 
 
 ;; Increase minibuffer history length.
